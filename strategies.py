5d5399db2b84ab3d6d8028884b99955c4bdc2c26
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2019-08-18 10:58:20 +0900

    表示を修正

--- strategies.py
+++ strategies.py
@@ -9,12 +9,9 @@ def console_user_input(stone, board):
     コンソールからのユーザ入力
     """
     possibles = list(board.get_possibles(stone).keys())
-    color = "黒" if stone == board.BLACK else "白"
     select = None
 
     while True:
-        print(f'\n* {color}の番です')
-
         for index, value in enumerate(possibles, 1):
             coordinate = (chr(value[0] + 97), str(value[1] + 1))
             print(f'{index:2d}:', coordinate)
@@ -36,10 +33,12 @@ if __name__ == '__main__':
     board4 = Board()
     board4.print_board()
 
+    print('\n* 黒の番です')
     x, y = console_user_input(Board.BLACK, board4)
     board4.put_stone(Board.BLACK, x, y)
     board4.print_board()
 
+    print('\n* 白の番です')
     x, y = console_user_input(Board.WHITE, board4)
     board4.put_stone(Board.WHITE, x, y)
     board4.print_board()
