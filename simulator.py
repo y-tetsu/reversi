41b17944e491a27030da91eb4eeaa4b26f5af61a
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2019-08-24 22:32:44 +0900

    表示修正

--- simulator.py
+++ simulator.py
@@ -17,7 +17,37 @@ class Simulator:
         self.whites = whites
         self.matches = matches
         self.board_size = board_size
-        self.results = []
+        self.game_results = []
+        self.total = []
+
+    def __str__(self):
+        board_size = "\nSize : " + str(self.board_size) + "\n"
+        header = "           | " + " ".join([f'{key:10s}' for key in self.total]) + "| Total\n"
+        hr = "-----------------------------------------------------------------------\n"
+
+        body = ""
+        for key1 in self.total:
+            row = f'{key1:10s} | '
+            wins, matches = 0, 0
+
+            for key2 in self.total:
+                if key1 == key2:
+                    row += "------    "
+                    continue
+
+                wins += self.total[key1][key2]['wins']
+                matches += self.total[key1][key2]['matches']
+                ratio = self.total[key1][key2]['wins'] / self.total[key1][key2]['matches'] * 100
+                ratio = f'{ratio:3.1f}%'
+                row += f'{ratio:>6s}    '
+
+            row += "| "
+            ratio = wins / matches * 100
+            ratio = f'{ratio:3.1f}%'
+            row += f'{ratio:>6s} ({wins} / {matches})'
+            body += f'{row}\n'
+
+        return board_size + header + hr + body + hr
 
     def start(self):
         """
@@ -30,69 +60,45 @@ class Simulator:
             for _ in range(self.matches):
                 game = Game(Board(self.board_size), black, white, False)
                 game.play()
-                self.results.append(game.result)
+                self.game_results.append(game.result)
 
-    def print_result(self):
+        self._totalize_results()
+
+    def _totalize_results(self):
         """
-        シミュレーション結果の表示
+        結果の集計
 
         """
-        table = {}
+        total = {}
 
-        for result in self.results:
+        for result in self.game_results:
             win = result[0]
             black = result[1][0]
             white = result[1][1]
 
-            if black not in table:
-                table[black] = {}
+            if black not in total:
+                total[black] = {}
 
-            if white not in table[black]:
-                table[black][white] = {'matches': 0, 'wins': 0}
+            if white not in total[black]:
+                total[black][white] = {'matches': 0, 'wins': 0}
 
             if win == Game.BLACK_WIN:
-                table[black][white]['wins'] += 1
+                total[black][white]['wins'] += 1
 
-            table[black][white]['matches'] += 1
+            total[black][white]['matches'] += 1
 
-            if white not in table:
-                table[white] = {}
+            if white not in total:
+                total[white] = {}
 
-            if black not in table[white]:
-                table[white][black] = {'matches': 0, 'wins': 0}
+            if black not in total[white]:
+                total[white][black] = {'matches': 0, 'wins': 0}
 
             if win == Game.WHITE_WIN:
-                table[white][black]['wins'] += 1
+                total[white][black]['wins'] += 1
 
-            table[white][black]['matches'] += 1
+            total[white][black]['matches'] += 1
 
-        print("\nSize :", self.board_size)
-        print("           | " + " ".join([f'{key:10s}' for key in table]) + "| Total")
-        print("-----------------------------------------------------------------------")
-
-        for key1 in table:
-            print(f'{key1:10s} | ', end="")
-            wins, matches = 0, 0
-
-            for key2 in table:
-                if key1 == key2:
-                    print("------    ", end="")
-                    continue
-
-                wins += table[key1][key2]['wins']
-                matches += table[key1][key2]['matches']
-
-                ratio = table[key1][key2]['wins'] / table[key1][key2]['matches'] * 100
-                ratio = f'{ratio:3.1f}%'
-
-                print(f'{ratio:>6s}    ', end="")
-
-            print("| ", end="")
-            ratio = wins / matches * 100
-            ratio = f'{ratio:3.1f}%'
-            print(f'{ratio:>6s} ({wins} / {matches})')
-
-        print("-----------------------------------------------------------------------")
+        self.total = total
 
 
 if __name__ == '__main__':
@@ -109,8 +115,7 @@ if __name__ == '__main__':
     blacks = [Player(Board.BLACK, *character) for character in characters]
     whites = [Player(Board.WHITE, *character) for character in characters]
 
-    simulator = Simulator(blacks, whites, 1)
-    elapsed_time = timeit.timeit('simulator.start()', globals=globals(), number=1)
+    simulator = Simulator(blacks, whites, 25)
 
-    simulator.print_result()
-    print(elapsed_time, "(s)")
+    elapsed_time = timeit.timeit('simulator.start()', globals=globals(), number=1)
+    print(simulator, elapsed_time, "(s)")
