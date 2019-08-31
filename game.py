a2d84225d32534462b801d7ef8b891be56814ab8
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2019-08-31 22:48:30 +0900

    ディスプレイクラスに対応

--- game.py
+++ game.py
@@ -10,7 +10,7 @@ class Game:
     """
     BLACK_WIN, WHITE_WIN, DRAW = 0, 1, 2
 
-    def __init__(self, board, black, white, display=True):
+    def __init__(self, board, black, white, display):
         self.board = board
         self.black = black
         self.white = white
@@ -22,25 +22,27 @@ class Game:
         ゲームを開始する
         """
         if not self.result:
-            self._print_if_display(self.board)
+            self.display.start(self.board, self.black, self.white)
 
             while True:
                 playable = 0
                 foul_player = None
 
                 for player in [self.black, self.white]:
-                    if self.board.get_possibles(player.stone):
-                        self._print_if_display("\n" + player.name + " の番です")
+                    possibles = self.board.get_possibles(player.stone)
 
-                        if player.put_stone(self.board):
-                            x = chr(player.move[0] + 97)
-                            y = str(player.move[1] + 1)
-                            self._print_if_display((x, y), "に置きました")
-                            self._print_if_display(self.board)
-                            playable += 1
-                        else:
-                            foul_player = player
-                            break
+                    if not possibles:
+                        continue
+
+                    self.display.turn(player, possibles)
+                    captures = player.put_stone(self.board)
+
+                    if not captures:
+                        foul_player = player
+                        break
+
+                    self.display.move(player, captures, self.board, self.black, self.white)
+                    playable += 1
 
                 if foul_player:
                     self._foul(foul_player)
@@ -50,38 +52,36 @@ class Game:
                     self._judge()
                     break
 
-    def _print_if_display(self, *messages):
-        """
-        表示
-        """
-        if self.display:
-            print(*messages)
-
     def _foul(self, player):
         """
         反則負け
         """
         if player.stone == self.black.stone:
-            self._win(self.white.name, Game.WHITE_WIN)
+            self._win(self.white)
         else:
-            self._win(self.black.name, Game.BLACK_WIN)
+            self._win(self.black)
 
     def _judge(self):
         """
         結果判定
         """
         if self.board.black_num > self.board.white_num:
-            self._win(self.black.name, Game.BLACK_WIN)
+            self._win(self.black)
         elif self.board.white_num > self.board.black_num:
-            self._win(self.white.name, Game.WHITE_WIN)
+            self._win(self.white)
         else:
             self._draw()
 
-    def _win(self, name, result):
+    def _win(self, player):
         """
         勝ち
         """
-        self._print_if_display("\n" + name + " の勝ちです")
+        self.display.win(player)
+
+        if player.stone == Board.BLACK:
+            result = Game.BLACK_WIN
+        else:
+            result = Game.WHITE_WIN
 
         self.result = GameResult(
             result,
@@ -93,7 +93,7 @@ class Game:
         """
         引き分け
         """
-        self._print_if_display("\n引き分けです")
+        self.display.draw()
 
         self.result = GameResult(
             Game.DRAW,
@@ -117,6 +117,7 @@ class GameResult:
 if __name__ == '__main__':
     from board import Board
     from player import Player
+    from display import ConsoleDisplay
     import strategies
 
     class Foul():
@@ -126,9 +127,21 @@ if __name__ == '__main__':
     black = Player(Board.BLACK, "Random", strategies.Random())
     white = Player(Board.WHITE, "Foul", Foul())
 
-    game = Game(Board(4), black, white)
+    game = Game(Board(4), black, white, ConsoleDisplay())
     game.play()
 
+    print()
+    print(game.result.winlose)
+    print(game.result.black_name, game.result.black_num)
+    print(game.result.white_name, game.result.white_num)
+
+    black = Player(Board.BLACK, "Random", strategies.Random())
+    white = Player(Board.WHITE, "Unselfish", strategies.Unselfish())
+
+    game = Game(Board(4), black, white, ConsoleDisplay())
+    game.play()
+
+    print()
     print(game.result.winlose)
     print(game.result.black_name, game.result.black_num)
     print(game.result.white_name, game.result.white_num)
