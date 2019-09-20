bc892c0a911f220af6036adcf99c87bf0331ac75
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2019-09-20 23:20:33 +0900

    石の指定から色の指定に変更

--- display.py
+++ display.py
@@ -12,7 +12,7 @@ PLAYER_COLORS = ('black', 'white')
 
 class AbstractDisplay(metaclass=abc.ABCMeta):
     @abc.abstractmethod
-    def progress(self, board, black, white):
+    def progress(self, board, black_player, white_player):
         pass
 
     @abc.abstractmethod
@@ -40,12 +40,12 @@ class ConsoleDisplay(AbstractDisplay):
     """
     コンソールへの表示
     """
-    def progress(self, board, black, white):
+    def progress(self, board, black_player, white_player):
         """
         ゲームの進行の表示(スコア、ボード状態)
         """
-        score_b = str(black) + ":" + str(board.score[black.stone])
-        score_w = str(white) + ":" + str(board.score[white.stone])
+        score_b = str(black_player) + ":" + str(board.score['black'])
+        score_w = str(white_player) + ":" + str(board.score['white'])
 
         print(score_b, score_w)
         print(board)
@@ -94,7 +94,7 @@ class NoneDisplay(AbstractDisplay):
     """
     表示なし
     """
-    def progress(self, board, black, white):
+    def progress(self, board, black_player, white_player):
         pass
 
     def turn(self, player, possibles):
@@ -121,12 +121,12 @@ class WindowDisplay(AbstractDisplay):
         self.info = window.info
         self.board = window.board
 
-    def progress(self, board, black, white):
+    def progress(self, board, black_player, white_player):
         """
         ゲームの進行の表示(スコア)
         """
-        for color, stone in zip(PLAYER_COLORS, (black.stone, white.stone)):
-            self.info.set_text(color, 'score', str(board.score[stone]))
+        for color in PLAYER_COLORS:
+            self.info.set_text(color, 'score', str(board.score[color]))
 
     def turn(self, player, possibles):
         """
@@ -135,7 +135,7 @@ class WindowDisplay(AbstractDisplay):
         for color in PLAYER_COLORS:
             self.info.set_text(color, 'move', '')  # 打った手の表示を消す
 
-        color = 'black' if player.stone == self.board.black else 'white'
+        color = 'black' if player.stone == self.board.stone['black'] else 'white'
         self.info.set_text(color, 'turn', '手番です')  # 手番の表示
         self.board.enable_moves(possibles)  # 打てる候補を表示
         time.sleep(0.3)
@@ -154,7 +154,7 @@ class WindowDisplay(AbstractDisplay):
         self.board.enable_move(*player.move)  # 打った手をハイライト
         self.board.put_stone(player.stone, *player.move)  # 石を置く
         time.sleep(0.3)
-        color = 'black' if player.stone == self.board.black else 'white'
+        color = 'black' if player.stone == self.board.stone['black'] else 'white'
         self.info.set_text(color, 'move', f'({x}, {y}) に置きました')  # 打った手を表示
         self.board.turn_stone(player.stone, player.captures)  # 石をひっくり返すアニメーション
         self.board.disable_move(*player.move)
@@ -163,14 +163,14 @@ class WindowDisplay(AbstractDisplay):
         """
         反則プレイヤーの表示
         """
-        color = 'black' if player.stone == self.board.black else 'white'
+        color = 'black' if player.stone == self.board.stone['black'] else 'white'
         self.info.set_text(color, 'winlose', '反則')
 
     def win(self, player):
         """
         勝ちプレイヤーの表示
         """
-        winner, loser = ('black', 'white') if player.stone == self.board.black else ('white', 'black')
+        winner, loser = ('black', 'white') if player.stone == self.board.stone['black'] else ('white', 'black')
         self.info.set_text(winner, 'winlose', '勝ち')
         self.info.set_text(loser,  'winlose', '負け')
 
@@ -188,19 +188,19 @@ if __name__ == '__main__':
     import strategies
 
     board8x8 = Board()
-    black = Player(board8x8.black, "Random", strategies.Random())
-    white = Player(board8x8.white, "User", strategies.ConsoleUserInput())
+    black_player = Player('black', "Random", strategies.Random())
+    white_player = Player('white', "User", strategies.ConsoleUserInput())
 
     display = ConsoleDisplay()
-    display.progress(board8x8, black, white)
+    display.progress(board8x8, black_player, white_player)
 
-    possibles = board8x8.get_possibles(black.stone)
-    display.turn(black, possibles)
+    possibles = board8x8.get_possibles('black')
+    display.turn(black_player, possibles)
 
-    black.put_stone(board8x8)
-    display.move(black, possibles)
-    display.progress(board8x8, black, white)
+    black_player.put_stone(board8x8)
+    display.move(black_player, possibles)
+    display.progress(board8x8, black_player, white_player)
 
-    display.foul(black)
-    display.win(black)
+    display.foul(black_player)
+    display.win(black_player)
     display.draw()
