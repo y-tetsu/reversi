b2c867f033419235881dacbb9fc7bee9bdf39a55
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2019-09-28 00:57:39 +0900

    評価テーブルの初期化追加

--- strategies.py
+++ strategies.py
@@ -141,6 +141,82 @@ class SlowStarter(AbstractStrategy):
         return self.unselfish.next_move(color, board)
 
 
+class Table(AbstractStrategy):
+    """
+    評価関数で手を決める(なるべく少なく取る、角を狙う、角のそばは避ける)
+    """
+    def __init__(self, board_size):
+        self.table = [[-1 for _ in range(board_size)] for _ in range(board_size)]
+
+        # 中
+        for num in range(0, board_size//2, 2):
+            if num != board_size//2 - 1:
+                self.table[num][num] = 0
+                self.table[num][board_size-num-1] = 0
+                self.table[board_size-num-1][board_size-num-1] = 0
+                self.table[board_size-num-1][num] = 0
+
+        for y in range(1, board_size//2-1, 2):
+            for x in range(1, board_size//2-1, 2):
+                if x == y:
+                    self.table[y][x] = -25
+                    self.table[y][board_size-x-1] = -25
+                    self.table[board_size-y-1][board_size-x-1] = -25
+                    self.table[board_size-y-1][x] = -25
+
+        for y in range(1, board_size//2-2, 2):
+            for x in range(y+1, board_size-y-1):
+                self.table[y][x] = -3
+                self.table[x][y] = -3
+                self.table[board_size-y-1][x] = -3
+                self.table[x][board_size-y-1] = -3
+
+        # 端
+        x_min, y_min = 0, 0
+        x_max, y_max = board_size - 1, board_size - 1
+
+        if board_size >= 6:
+            for y in range(board_size):
+                for x in range(board_size):
+                    if x == x_min or y == y_min or x == x_max or y == y_max:
+                        if x == x_min and y == y_min:
+                            self.table[y][x] = 50
+                            self.table[y][x+1] = -12
+                            self.table[y][x+2] = 0
+                            self.table[y+1][x] = -12
+                            self.table[y+2][x] = 0
+
+                        if x == x_min and y == y_max:
+                            self.table[y][x] = 50
+                            self.table[y][x+1] = -12
+                            self.table[y][x+2] = 0
+                            self.table[y-1][x] = -12
+                            self.table[y-2][x] = 0
+
+                        if x == x_max and y == y_min:
+                            self.table[y][x] = 50
+                            self.table[y][x-1] = -12
+                            self.table[y][x-2] = 0
+                            self.table[y+1][x] = -12
+                            self.table[y+2][x] = 0
+
+                        if x == x_max and y == y_max:
+                            self.table[y][x] = 50
+                            self.table[y][x-1] = -12
+                            self.table[y][x-2] = 0
+                            self.table[y-1][x] = -12
+                            self.table[y-2][x] = 0
+
+        for row in self.table:
+            print(row)
+
+    def next_move(self, color, board):
+        """
+        次の一手
+        """
+        return (0, 0)
+
+
 if __name__ == '__main__':
     def input(string):
         print(string + '1')
@@ -187,3 +263,20 @@ if __name__ == '__main__':
         if not cnt:
             print('\n終了')
             break
+
+    test4 = Player('white', 'Table', Table(4))
+    print()
+    test6 = Player('white', 'Table', Table(6))
+    print()
+    test8 = Player('white', 'Table', Table(8))
+    print()
+    test10 = Player('white', 'Table', Table(10))
+    print()
+    test12 = Player('white', 'Table', Table(12))
+    print()
+    test14 = Player('white', 'Table', Table(14))
+    print()
+    test16 = Player('white', 'Table', Table(16))
+    print()
+    test26 = Player('white', 'Table', Table(26))
+    print()
