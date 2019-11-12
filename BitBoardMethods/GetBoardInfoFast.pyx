35ef191544d9f1d6b18a3c4e178397b7238bc672
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2019-11-12 20:59:05 +0900

    GetBoardInfoFast追加

---
+++ BitBoardMethods/GetBoardInfoFast.pyx
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+"""
+ビットボードの情報取得処理
+"""
+
+def get_board_info(size, b, w):
+    """
+    ボードの情報(黒:1、白:-1、空き:0)を返す
+    """
+    board_info = []
+    mask = 1 << (size * size - 1)
+
+    for y in range(size):
+        tmp = []
+        for x in range(size):
+            if b & mask:
+                tmp.append(1)
+            elif w & mask:
+                tmp.append(-1)
+            else:
+                tmp.append(0)
+            mask >>= 1
+        board_info.append(tmp)
+
+    return board_info
--- BitBoardMethods/setup.py
+++ BitBoardMethods/setup.py
@@ -3,10 +3,20 @@ from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 
+# GetPsossiblesFast
 ext_modules = [Extension("GetPossiblesFast", ["GetPossiblesFast.pyx"])]
 
 setup(
   name = 'GetPossiblesFast',
+  cmdclass = {'build_ext': build_ext},
+  ext_modules = ext_modules
+)
+
+# GetBoardInfoFast
+ext_modules = [Extension("GetBoardInfoFast", ["GetBoardInfoFast.pyx"])]
+
+setup(
+  name = 'GetBoardInfoFast',
   cmdclass = {'build_ext': build_ext},
   ext_modules = ext_modules
 )
