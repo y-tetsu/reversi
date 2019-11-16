fd8fddabb9ce66bca5c8ed5a5de708156678d723
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2019-11-16 22:23:25 +0900

    石クラス見直し

--- stone.py
+++ stone.py
@@ -4,10 +4,6 @@
 """
 
 
-BLACK, WHITE, BLANK = (('BLACK', '〇'), ('WHITE', '●'), ('BLANK', '□'))
-ALL_STONES = (BLACK, WHITE, BLANK)
-
-
 class Stone(str):
     """
     石の基底クラス
@@ -15,23 +11,36 @@ class Stone(str):
     __slots__ = ()  # データを持たない
 
 
+class Black(Stone):
+    pass
+
+
+class White(Stone):
+    pass
+
+
+class Blank(Stone):
+    pass
+
+
 class StoneFactory:
     """
     石ファクトリ
     """
-    def __init__(self):
-        # 黒、白、空クラス生成
-        for color, mark in ALL_STONES:
-            new = (lambda m: lambda Class: Stone.__new__(Class, m))(mark)
-            new.__name__ = '__new__'  # クラスに使うので名前を<lambda>から__new__へ変更
-            Class = type(color.title(), (Stone,), dict(__slots__=(), __new__=new))
-            globals()[color.title()] = Class
-
     def create(self, color):
         """
         石オブジェクトを生成する
         """
-        return globals()[color.title()]()
+        ret = ''
+
+        if color == 'black':
+            ret = Black('〇')
+        elif color == 'white':
+            ret = White('●')
+        elif color == 'blank':
+            ret = Blank('□')
+
+        return ret
 
 
 if __name__ == '__main__':
