872c3ca6d25ea3aa38da0b50462fa1f9accdd181
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2019-12-27 18:10:59 +0900

    選択的探索用基底クラス

--- strategies/common.py
+++ strategies/common.py
@@ -14,12 +14,20 @@ class AbstractStrategy(metaclass=abc.ABCMeta):
     def next_move(self, color, board):
         pass
 
+
 class AbstractScorer(metaclass=abc.ABCMeta):
     @abc.abstractmethod
     def get_score(self, color, board):
         pass
 
+
 class AbstractEvaluator(metaclass=abc.ABCMeta):
     @abc.abstractmethod
     def evaluate(self, color, board):
+        pass
+
+
+class AbstractSelector(metaclass=abc.ABCMeta):
+    @abc.abstractmethod
+    def select_moves(self, color, board, best_move, scores):
         pass
