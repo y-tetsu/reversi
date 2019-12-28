4dcd1fe2717df283dcd18ff5f44c0351984a84ab
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2019-12-28 22:04:44 +0900

    最も低い手を捨てる

--- strategies/selector.py
+++ strategies/selector.py
@@ -58,8 +58,35 @@ class Selector_BC(Selector_B):
         return moves
 
 
+class Selector_BCW(Selector_BC):
+    """
+    最もスコアの低い手を捨てる
+    """
+    def __init__(self, depth=5):
+        self.depth = depth
+
+    def select_moves(self, color, board, best_move, scores, depth):
+        """
+        手の候補を決める
+        """
+        moves = super().select_moves(color, board, best_move, scores, depth)
+
+        if depth >= self.depth:  # 一定以上の深さの場合
+            worst_score = min([score for score in scores.values()])
+            worst_moves = [key for key in scores.keys() if scores[key] == worst_score]
+
+            # 打てる手の数がスコアの低い手の数より多い場合
+            if len(moves) > len(worst_moves):
+                for worst_move in worst_moves:
+                    moves.remove(worst_move)  # 最もスコアの低い手を削除
+
+        return moves
+
+
 if __name__ == '__main__':
     from board import BitBoard
+    from strategies.timer import Timer
+    from strategies.alphabeta import AlphaBeta_TPOW
 
     bitboard8 = BitBoard(8)
     bitboard8.put_stone('black', 3, 2)
@@ -94,3 +121,24 @@ if __name__ == '__main__':
     moves = selector.select_moves('black', bitboard8, (2, 3), None, None)
     print(moves)
     assert moves == [(0, 7), (2, 3), (0, 3), (0, 4), (5, 4), (0, 5), (4, 5), (5, 5), (0, 6), (2, 7)]
+
+    print('--- Test For Selector_BCW ---')
+    print(bitboard8)
+
+    strategy = AlphaBeta_TPOW()
+    selector = Selector_BCW()
+
+    assert selector.depth == 5
+
+    moves = bitboard8.get_possibles('black')
+    print(moves)
+
+    Timer.set_deadline(strategy.__class__.__name__, 0.5)
+    best_move, scores = strategy.get_best_move('black', bitboard8, moves, 4)
+
+    moves = selector.select_moves('black', bitboard8, best_move, scores, 4)
+    print(moves)
+    assert moves == [(0, 7), (5, 5), (0, 3), (2, 3), (0, 4), (5, 4), (0, 5), (4, 5), (0, 6), (2, 7)]
+    moves = selector.select_moves('black', bitboard8, best_move, scores, 5)
+    print(moves)
+    assert moves == [(0, 7), (5, 5), (2, 3), (0, 4), (5, 4), (0, 5), (4, 5), (0, 6), (2, 7)]
