f0bda99024b0e5487c2ae9aa696b2cbb68e12603
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2020-02-03 00:10:37 +0900

    サンプル入力追加

--- src/strategies/external_samples/python_sample.py
+++ src/strategies/external_samples/python_sample.py
@@ -1,7 +1,24 @@
 #!/usr/bin/env python
 import sys
-import time
 
-message = sys.stdin.read()
-#time.sleep(1.5)
-print('a 2')
+def get_message():
+    """
+    標準入力の読み込み
+    """
+    lines = sys.stdin.read().split('\n')
+    color = int(lines.pop(0))
+    size = int(lines.pop(0))
+    board = [[int(i) for i in line.split()] for line in lines]
+
+    return (color, size, board)
+
+
+if __name__ == '__main__':
+    # 標準入力を受ける
+    color, size, board = get_message()
+    print(color, file=sys.stderr)
+    print(size, file=sys.stderr)
+    print(board, file=sys.stderr)
+
+    # 結果を標準出力
+    print('0 0')
