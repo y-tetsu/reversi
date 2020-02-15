ae2e9f20280620edd9c9606437520ef643b401a5
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2020-02-15 21:46:10 +0900

    Evaluator_TPWE追加

--- src/strategies/coordinator/evaluator.py
+++ src/strategies/coordinator/evaluator.py
@@ -7,7 +7,7 @@ import sys
 sys.path.append('../../')
 
 from strategies.common import AbstractEvaluator
-from strategies.coordinator import TableScorer, PossibilityScorer, OpeningScorer, WinLoseScorer, NumberScorer
+from strategies.coordinator import TableScorer, PossibilityScorer, OpeningScorer, WinLoseScorer, NumberScorer, EdgeScorer
 
 
 class Evaluator_T(AbstractEvaluator):
@@ -72,6 +72,20 @@ class Evaluator_N(AbstractEvaluator):
     """
     def __init__(self):
         self.scorer = NumberScorer()  # 石数による評価値算出
+
+    def evaluate(self, *args, **kwargs):
+        """
+        評価値の算出
+        """
+        return self.scorer.get_score(kwargs['board'])
+
+
+class Evaluator_E(AbstractEvaluator):
+    """
+    辺のパターンの評価値を算出
+    """
+    def __init__(self, wpy=100):
+        self.scorer = EdgeScorer(wpy)  # 辺のパターンによる評価値算出
 
     def evaluate(self, *args, **kwargs):
         """
@@ -216,10 +230,37 @@ class Evaluator_TPOW(Evaluator_TPO):
         return score_t + score_p + score_o
 
 
-if __name__ == '__main__':
-    from board import Board
+class Evaluator_TPWE(AbstractEvaluator):
+    """
+    盤面の評価値をTable+配置可能数+勝敗+辺のパターンで算出
+    """
+    def __init__(self, size=8, corner=50, c=-20, a1=0, a2=-1, b=-1, x=-25, o=-5, wp=5, ww=10000, wpy=100):
+        self.t = Evaluator_T(size, corner, c, a1, a2, b, x, o)
+        self.p = Evaluator_P(wp)
+        self.w = Evaluator_W(ww)
+        self.e = Evaluator_E(wpy)
 
-    board8 = Board(8)
+    def evaluate(self, *args, **kwargs):
+        """
+        評価値の算出
+        """
+        score_w = self.w.evaluate(*args, **kwargs)
+
+        # 勝敗が決まっている場合
+        if score_w is not None:
+            return score_w
+
+        score_t = self.t.evaluate(*args, **kwargs)
+        score_p = self.p.evaluate(*args, **kwargs)
+        score_e = self.e.evaluate(*args, **kwargs)
+
+        return score_t + score_p + score_e
+
+
+if __name__ == '__main__':
+    from board import BitBoard
+
+    board8 = BitBoard(8)
     board8.put_stone('black', 3, 2)
     board8.put_stone('white', 2, 2)
     board8.put_stone('black', 2, 3)
@@ -301,3 +342,16 @@ if __name__ == '__main__':
     score = evaluator.evaluate(board=board8, possibles_b=possibles_b, possibles_w=possibles_w)
     print('score', score)
     assert score == -6
+
+    #----------------------------------------------------------------
+    # Evaluator_TPWE
+    evaluator = Evaluator_TPWE()
+
+    board8._black_bitboard = 0x0000002010003C7E
+    possibles_b = board8.get_possibles('black', True)
+    possibles_w = board8.get_possibles('white', True)
+
+    score = evaluator.evaluate(color='black', board=board8, possibles_b=possibles_b, possibles_w=possibles_w)
+    print(board8)
+    print('score', score)
+    assert score == 19
