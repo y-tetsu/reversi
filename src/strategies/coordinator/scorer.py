5da0f6ae1199ffd121ce0d378fce29b06ec4fcda
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2020-02-15 21:22:32 +0900

    EdgeScorerにピュア山のスコア値追加

--- src/strategies/coordinator/scorer.py
+++ src/strategies/coordinator/scorer.py
@@ -131,6 +131,44 @@ class NumberScorer(AbstractScorer):
         return board.score['black'] - board.score['white']
 
 
+class EdgeScorer(AbstractScorer):
+    """
+    辺のパターンに基づいて算出
+    """
+    def __init__(self, w1=100):
+        self._W1 = w1
+
+        # ピュア山
+        self.pureyama_mask = [0xFF7E000000000000, 0x0103030303030301, 0x0000000000007EFF, 0x80C0C0C0C0C0C080]
+        self.pureyama_value = [0x7E3C000000000000, 0x0001030303030100, 0x0000000000003C7E, 0x0080C0C0C0C08000]
+
+    def get_score(self, board):
+        """
+        評価値の算出
+        """
+        score = 0
+        b_bitboard, w_bitboard = board.get_bitboard_info()
+
+        # ボードサイズ8以外は考慮なし
+        if board.size != 8:
+            return score
+
+        # ピュア山値
+        for mask, value in zip(self.pureyama_mask, self.pureyama_value):
+            score += self._get_mask_value(b_bitboard, w_bitboard, mask, value, self._W1)
+
+        return score
+
+    def _get_mask_value(self, b_bitboard, w_bitboard, mask, value, weight):
+        """
+        マスクした値を取得
+        """
+        score_b = weight if (b_bitboard & mask) == value else 0
+        score_w = weight if (w_bitboard & mask) == value else 0
+
+        return score_b - score_w
+
+
 if __name__ == '__main__':
     from board import BitBoard
 
@@ -186,3 +224,37 @@ if __name__ == '__main__':
 
     print('score', scorer.get_score(board8))
     assert scorer.get_score(board8) == -6
+
+    #------------------------------------------------------
+    # EdgeScorer
+    scorer = EdgeScorer()
+
+    # ピュア山
+    board8 = BitBoard(8)
+    board8._black_bitboard = 0x7E3C000000000000
+    board8._white_bitboard = 0x0000000000000000
+    print(board8)
+    score = scorer.get_score(board8)
+    print('score', score)
+    assert score == 100
+
+    board8._black_bitboard = 0xFE3C000000000000
+    board8._white_bitboard = 0x0000000000000000
+    print(board8)
+    score = scorer.get_score(board8)
+    print('score', score)
+    assert score == 0
+
+    board8._black_bitboard = 0x7E3C000000003C7E
+    board8._white_bitboard = 0x0000000000000000
+    print(board8)
+    score = scorer.get_score(board8)
+    print('score', score)
+    assert score == 200
+
+    board8._black_bitboard = 0x0000000000000000
+    board8._white_bitboard = 0x0081C3C3C3C38100
+    print(board8)
+    score = scorer.get_score(board8)
+    print('score', score)
+    assert score == -200
