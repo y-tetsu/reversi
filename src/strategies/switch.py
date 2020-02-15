8196f4d176793e7c30bd225e0328c826b6424817
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2020-02-16 01:50:12 +0900

    パラメータ調整中

--- src/strategies/switch.py
+++ src/strategies/switch.py
@@ -86,9 +86,9 @@ class SwitchNsI_B_TPWE(Switch):
             ],
             strategies=[
                 IterativeDeepning(depth=2, selector=Selector(), sorter=Sorter_B(), search=NegaScout(evaluator=Evaluator_TPW (corner=100, c=-20, a1= 0, a2=-1, b=-1, x=-25, o=-5, wp= 5))),
-                IterativeDeepning(depth=2, selector=Selector(), sorter=Sorter_B(), search=NegaScout(evaluator=Evaluator_TPWE(corner= 70, c=-20, a1= 0, a2=-1, b=-1, x=-25, o=-5, wp= 5, wpy=50, wy=100))),
-                IterativeDeepning(depth=2, selector=Selector(), sorter=Sorter_B(), search=NegaScout(evaluator=Evaluator_TPWE(corner= 50, c=-20, a1= 0, a2=-1, b=-1, x=-25, o=-5, wp= 5, wpy=50, wy=100))),
-                IterativeDeepning(depth=2, selector=Selector(), sorter=Sorter_B(), search=NegaScout(evaluator=Evaluator_TPWE(corner= 30, c=  0, a1= 1, a2= 1, b= 1, x=  0, o= 1, wp= 6, wpy=50, wy=100))),
+                IterativeDeepning(depth=2, selector=Selector(), sorter=Sorter_B(), search=NegaScout(evaluator=Evaluator_TPWE(corner= 70, c=-20, a1= 0, a2=-1, b=-1, x=-25, o=-5, wp= 5, wpy=50, wy=20))),
+                IterativeDeepning(depth=2, selector=Selector(), sorter=Sorter_B(), search=NegaScout(evaluator=Evaluator_TPWE(corner= 50, c=-20, a1= 0, a2=-1, b=-1, x=-25, o=-5, wp= 5, wpy=50, wy=20))),
+                IterativeDeepning(depth=2, selector=Selector(), sorter=Sorter_B(), search=NegaScout(evaluator=Evaluator_TPWE(corner= 30, c=  0, a1= 1, a2= 1, b= 1, x=  0, o= 1, wp= 6, wpy=50, wy=20))),
                 IterativeDeepning(depth=2, selector=Selector(), sorter=Sorter_B(), search=NegaScout(evaluator=Evaluator_TPW (corner=  5, c=  0, a1= 1, a2= 1, b= 1, x=  0, o= 1, wp= 8)))
             ]):
         super().__init__(turns, strategies)
