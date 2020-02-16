86be09259f9a493e35967abc1cc7afc16e361392
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2020-02-16 18:53:02 +0900

    ウィングの評価値を追加

--- src/strategies/coordinator/evaluator.py
+++ src/strategies/coordinator/evaluator.py
@@ -84,8 +84,8 @@ class Evaluator_E(AbstractEvaluator):
     """
     辺のパターンの評価値を算出
     """
-    def __init__(self, wpy=50, wy=100):
-        self.scorer = EdgeScorer(wpy, wy)  # 辺のパターンによる評価値算出
+    def __init__(self, wpy=50, wy=100, wpwin=-15, wwin=-30):
+        self.scorer = EdgeScorer(wpy, wy, wpwin, wwin)  # 辺のパターンによる評価値算出
 
     def evaluate(self, *args, **kwargs):
         """
@@ -234,11 +234,11 @@ class Evaluator_TPWE(AbstractEvaluator):
     """
     盤面の評価値をTable+配置可能数+勝敗+辺のパターンで算出
     """
-    def __init__(self, size=8, corner=50, c=-20, a1=0, a2=-1, b=-1, x=-25, o=-5, wp=5, ww=10000, wpy=50, wy=100):
+    def __init__(self, size=8, corner=50, c=-20, a1=0, a2=-1, b=-1, x=-25, o=-5, wp=5, ww=10000, wpy=50, wy=100, wpwin=-15, wwin=-30):
         self.t = Evaluator_T(size, corner, c, a1, a2, b, x, o)
         self.p = Evaluator_P(wp)
         self.w = Evaluator_W(ww)
-        self.e = Evaluator_E(wpy, wy)
+        self.e = Evaluator_E(wpy, wy, wpwin, wwin)
 
     def evaluate(self, *args, **kwargs):
         """
@@ -355,3 +355,9 @@ if __name__ == '__main__':
     print(board8)
     print('score', score)
     assert score == 69
+
+    board8._black_bitboard = 0x0000002010003C7C
+    score = evaluator.evaluate(color='black', board=board8, possibles_b=possibles_b, possibles_w=possibles_w)
+    print(board8)
+    print('score', score)
+    assert score == -106
