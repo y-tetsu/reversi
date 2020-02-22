39ab778a74fe2e0fb1a266e43e22d6786b9e6f39
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2020-02-22 16:12:33 +0900

    山のマスク修正

--- src/strategies/minmax.py
+++ src/strategies/minmax.py
@@ -197,7 +197,7 @@ class MinMax2_TPWE(MinMax):
     """
     MinMax法でEvaluator_TPWEにより次の手を決める(2手読み)
     """
-    def __init__(self, depth=2, evaluator=Evaluator_TPWE()):
+    def __init__(self, depth=2, evaluator=Evaluator_TPWE(corner=50, c=-20, a1=0, a2=-1, b=-1, x=-25, o=-5, wp=5, ww=10000, wpy=0, wy=3, wpwin=0, wwin=0, wb=0)):
         super().__init__(depth, evaluator)
 
 
