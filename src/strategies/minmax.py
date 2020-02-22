4cdf04b5d870bb299afb7b4aa4583e504dcb3e22
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2020-02-22 22:26:04 +0900

    Edgeの重み検討用

--- src/strategies/minmax.py
+++ src/strategies/minmax.py
@@ -189,7 +189,7 @@ class MinMax1_TPWE(MinMax):
     """
     MinMax法でEvaluator_TPWEにより次の手を決める(1手読み)
     """
-    def __init__(self, depth=1, evaluator=Evaluator_TPWE(corner=50, c=-20, a1=0, a2=-1, b=-1, x=-25, o=-5, wp=5, ww=10000, wpy=0, wy=0, wpwin=0, wwin=0, wb=0)):
+    def __init__(self, depth=1, evaluator=Evaluator_TPWE(corner=50, c=-20, a1=0, a2=-1, b=-1, x=-25, o=-5, wp=5, ww=10000, wpy=47, wy=28, wpwin=0, wwin=0, wb=0)):
         super().__init__(depth, evaluator)
 
 
