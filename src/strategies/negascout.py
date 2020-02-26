cf4123e4e71dc19b8f2e4c5a968226c4f444872f
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2020-02-26 21:05:46 +0900

    パラメータ修正

--- src/strategies/negascout.py
+++ src/strategies/negascout.py
@@ -78,7 +78,7 @@ class NegaScout_TPWE(NegaScout):
     """
     NegaScout法でEvaluator_TPWEにより次の手を決める
     """
-    def __init__(self, evaluator=Evaluator_TPWE()):
+    def __init__(self, evaluator=Evaluator_TPWE(corner=50, c=-20, a1=0, a2=-1, b=-1, x=-25, o=-5, wp=5, ww=10000, wpy=47, wy=28, wpwin=0, wwin=-3, wb=0, ws1=100, ws2=100, ws3=100, ws4=100, ws5=100, ws6=100, ws7=100)):
         super().__init__(evaluator=evaluator)
 
 
