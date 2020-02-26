84057144efa1ee117d8785a5b731f39876d9e8b3
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2020-02-26 22:33:12 +0900

    パラメータ調整

--- src/strategies/minmax.py
+++ src/strategies/minmax.py
@@ -189,7 +189,7 @@ class MinMax1_TPW2(MinMax):
     """
     MinMax法でEvaluator_TPWにより次の手を決める(1手読み)
     """
-    def __init__(self, depth=1, evaluator=Evaluator_TPW(corner=50, c=-20, a1=0, a2=10, b=-1, x=-25, o=-5, wp=5, ww=10000)):
+    def __init__(self, depth=1, evaluator=Evaluator_TPW(corner=50, c=-20, a1=0, a2=22, b=-1, x=-25, o=-5, wp=5, ww=10000)):
         super().__init__(depth, evaluator)
 
 
