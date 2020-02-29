3669ba6959493f3d8cb74cbd519279beb63aa85b
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2020-02-29 11:33:39 +0900

    AlphaBeta整理

--- src/strategies/randomopening.py
+++ src/strategies/randomopening.py
@@ -11,11 +11,11 @@ from strategies.easy import Random
 from strategies.proto import AB_TI
 from strategies.minmax import MinMax1_TPW, MinMax1_TPW2, MinMax1_TPWE, MinMax1_TPWEC, MinMax2_T, MinMax2_TPW, MinMax2_TPWE, MinMax3_T, MinMax3_TP, MinMax3_TPW, MinMax3_TPOW
 from strategies.negamax import _NegaMax3_TPW, NegaMax3_TPW, NegaMax3_TPOW
-from strategies.alphabeta import AlphaBeta3_TPW, AlphaBeta3_TPOW, AlphaBeta4_TPW, AlphaBeta4_TPWE
+from strategies.alphabeta import AlphaBeta4_TPW, AlphaBeta4_TPWE
 from strategies.negascout import NegaScout3_TPW, NegaScout3_TPOW, NegaScout4_TPW, NegaScout4_TPWE
 from strategies.iterative import AbI_B_TPW, NsI_B_TPW, NsI_BC_TPW, NsI_CB_TPW
 from strategies.fullreading import AlphaBeta4F9_TPW, AbIF9_B_TPW, NsIF9_B_TPW
-from strategies.joseki import AlphaBeta4F9J_TPW, AbIF9J_B_TPW, AbIF9J_BC_TPW, AbIF11J_B_TPW, AbIF11J_BC_TPW, NsIF9J_B_TPW, NsIF9J_B_TPW2, NsIF9J_B_TPWE, SwitchNsIF9J_B_TPW, SwitchNsIF9J_B_TPWE
+from strategies.joseki import AlphaBeta4F9J_TPW, AbIF9J_B_TPW, NsIF9J_B_TPW, NsIF9J_B_TPW2, NsIF9J_B_TPWE, SwitchNsIF9J_B_TPW, SwitchNsIF9J_B_TPWE
 
 
 class RandomOpening(AbstractStrategy):
@@ -153,22 +153,6 @@ class NegaMax3Ro_TPOW(RandomOpening):
         super().__init__(depth, base)
 
 
-class AlphaBeta3Ro_TPW(RandomOpening):
-    """
-    RandamOpening(8手) + AlphaBeta3_TPW
-    """
-    def __init__(self, depth=8, base=AlphaBeta3_TPW()):
-        super().__init__(depth, base)
-
-
-class AlphaBeta3Ro_TPOW(RandomOpening):
-    """
-    RandamOpening(8手) + AlphaBeta3_TPOW
-    """
-    def __init__(self, depth=8, base=AlphaBeta3_TPOW()):
-        super().__init__(depth, base)
-
-
 class AlphaBeta4Ro_TPW(RandomOpening):
     """
     RandamOpening(8手) + AlphaBeta4_TPW
@@ -297,30 +281,6 @@ class AbIF9JRo_B_TPW(RandomOpening):
         super().__init__(depth, base)
 
 
-class AbIF9JRo_BC_TPW(RandomOpening):
-    """
-    RandamOpening(8手) + AbIF9J_BC_TPW
-    """
-    def __init__(self, depth=8, base=AbIF9J_BC_TPW()):
-        super().__init__(depth, base)
-
-
-class AbIF11JRo_B_TPW(RandomOpening):
-    """
-    RandamOpening(8手) + AbIF11J_B_TPW
-    """
-    def __init__(self, depth=8, base=AbIF11J_B_TPW()):
-        super().__init__(depth, base)
-
-
-class AbIF11JRo_BC_TPW(RandomOpening):
-    """
-    RandamOpening(8手) + AbIF11J_BC_TPW
-    """
-    def __init__(self, depth=8, base=AbIF11J_BC_TPW()):
-        super().__init__(depth, base)
-
-
 class NsIF9JRo_B_TPW(RandomOpening):
     """
     RandamOpening(8手) + NsIF9J_B_TPW
@@ -378,7 +338,7 @@ if __name__ == '__main__':
     bitboard8.put_stone('black', 4, 6)
     print(bitboard8)
 
-    randomopening = AbIF11JRo_B_TPW()
+    randomopening = AbIF9JRo_B_TPW()
     move = randomopening.next_move('white', bitboard8)
     print(move)
 
