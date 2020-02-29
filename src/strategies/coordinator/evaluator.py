1e14f02b984e55b270f35a61e86715fd2bd3cebd
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2020-02-29 11:09:38 +0900

    デフォルト値修正

--- src/strategies/coordinator/evaluator.py
+++ src/strategies/coordinator/evaluator.py
@@ -248,7 +248,7 @@ class Evaluator_TPWE(AbstractEvaluator):
     """
     盤面の評価値をTable+配置可能数+勝敗+辺のパターンで算出
     """
-    def __init__(self, size=8, corner=50, c=-20, a1=0, a2=-1, b=-1, x=-25, o=-5, wp=5, ww=10000, wpy=47, wy=28, wpwin=0, wwin=-3, wb=0, ws1=10, ws2=10, ws3=10, ws4=10, ws5=10, ws6=10, ws7=10):
+    def __init__(self, size=8, corner=50, c=-20, a1=0, a2=-1, b=-1, x=-25, o=-5, wp=5, ww=10000, wpy=47, wy=28, wpwin=0, wwin=-3, wb=0, ws1=100, ws2=100, ws3=100, ws4=100, ws5=100, ws6=100, ws7=100):
         self.t = Evaluator_T(size, corner, c, a1, a2, b, x, o)
         self.p = Evaluator_P(wp)
         self.w = Evaluator_W(ww)
