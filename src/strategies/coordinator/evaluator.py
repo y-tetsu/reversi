689338ef3d991d8aaef7777236aea838f659d39c
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2020-03-04 21:24:31 +0900

    パラメータ修正

--- src/strategies/coordinator/evaluator.py
+++ src/strategies/coordinator/evaluator.py
@@ -275,7 +275,7 @@ class Evaluator_TPWEC(AbstractEvaluator):
     """
     盤面の評価値をTable+配置可能数+勝敗+辺のパターン+隅のパターンで算出
     """
-    def __init__(self, size=8, corner=50, c=-20, a1=0, a2=-1, b=-1, x=-25, o=-5, wp=5, ww=10000, we=100, wc=100):
+    def __init__(self, size=8, corner=50, c=-20, a1=0, a2=-1, b=-1, x=-25, o=-5, wp=5, ww=10000, we=100, wc=120):
         self.t = Evaluator_T(size, corner, c, a1, a2, b, x, o)
         self.p = Evaluator_P(wp)
         self.w = Evaluator_W(ww)
