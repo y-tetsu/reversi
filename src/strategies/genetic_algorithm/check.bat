b7b856a99c6402893280f32607278361af576e4b
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2020-04-27 18:07:50 +0900

    チェックツール追加

---
+++ src/strategies/genetic_algorithm/check.bat
@@ -0,0 +1,1 @@
+py -3.7 check.py > check.txt
---
+++ src/strategies/genetic_algorithm/check.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+"""
+経緯のチェック
+"""
+import os
+import json
+
+
+if __name__ == '__main__':
+    print('generation\tfitness\tcorner\tc\ta1\ta2\tb\to\tx\twp\twe')
+
+    i = 0
+    while True:
+        json_file = './population' + str(i) + '.json'
+        if os.path.isfile(json_file):
+            with open(json_file) as f:
+                population = json.load(f)
+                fitnesses = population['fitness']
+                fitness = max(fitnesses)
+                max_index = fitnesses.index(fitness)
+
+                corner = population['corner'][max_index]
+                c = population['c'][max_index]
+                a1 = population['a1'][max_index]
+                a2 = population['a2'][max_index]
+                b = population['b'][max_index]
+                o = population['o'][max_index]
+                x = population['x'][max_index]
+                wp = population['wp'][max_index]
+                we = population['we'][max_index]
+
+            text = "\t".join([str(param) for param in [i, fitness, corner, c, a1, a2, b, o, x, wp, we]])
+
+            print(text)
+        else:
+            break
+        i += 1
