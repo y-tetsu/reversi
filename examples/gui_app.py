56b98988fe574ab50f7255d082f00923019840b2
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2020-05-08 19:59:40 +0900

    GUIアプリ整理

--- examples/gui_app.py
+++ examples/gui_app.py
@@ -10,22 +10,20 @@ if '__file__' in globals():
 from reversi import Reversi, strategies
 
 
-reversi = Reversi(
-        {
-            'Unselfish': strategies.Unselfish(),
-            'Random': strategies.Random(),
-            'Greedy': strategies.Greedy(),
-            'SlowStarter': strategies.SlowStarter(),
-            'Table': strategies.Table(),
-            'MonteCarlo': strategies.MonteCarlo1000(),
-            'MinMax': strategies.MinMax2_TPW(),
-            'NegaMax': strategies.NegaMax3_TPW(),
-            'AlphaBeta': strategies.AlphaBeta4_TPW(),
-            'Joseki' : strategies.AlphaBeta4J_TPW(),
-            'FullReading' : strategies.AlphaBeta4F9J_TPW(),
-            'Iterative': strategies.AbIF9J_B_TPW(),
-            'Edge': strategies.AbIF9J_B_TPWE(),
-        }
-)
-
-reversi.start()
+Reversi(
+    {
+        'Unselfish': strategies.Unselfish(),
+        'Random': strategies.Random(),
+        'Greedy': strategies.Greedy(),
+        'SlowStarter': strategies.SlowStarter(),
+        'Table': strategies.Table(),
+        'MonteCarlo': strategies.MonteCarlo1000(),
+        'MinMax': strategies.MinMax2_TPW(),
+        'NegaMax': strategies.NegaMax3_TPW(),
+        'AlphaBeta': strategies.AlphaBeta4_TPW(),
+        'Joseki' : strategies.AlphaBeta4J_TPW(),
+        'FullReading' : strategies.AlphaBeta4F9J_TPW(),
+        'Iterative': strategies.AbIF9J_B_TPW(),
+        'Edge': strategies.AbIF9J_B_TPWE(),
+    }
+).start()
