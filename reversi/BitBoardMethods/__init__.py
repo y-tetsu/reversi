6e9b4192703b00fde6439dfd7eccc29db25f5030
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2020-05-10 09:32:11 +0900

    BitBoardMethods低速の区別用フラグ追加

--- reversi/BitBoardMethods/__init__.py
+++ reversi/BitBoardMethods/__init__.py
@@ -2,12 +2,17 @@
 import pyximport
 pyximport.install()
 
+SLOW_MODE1 = True
+SLOW_MODE2 = True
+
 try:
     from reversi.BitBoardMethods.GetLegalMovesFast import get_legal_moves
+    SLOW_MODE1 = False
 except ImportError:
     from reversi.BitBoardMethods.GetLegalMoves import get_legal_moves
 
 try:
     from reversi.BitBoardMethods.GetBoardInfoFast import get_board_info
+    SLOW_MODE2 = False
 except ImportError:
     from reversi.BitBoardMethods.GetBoardInfo import get_board_info
