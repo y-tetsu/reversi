ceec725d819afc5a088ef60d609281db3fd7237e
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2020-05-21 14:55:38 +0900

    mod setup.py

--- setup.py
+++ setup.py
@@ -11,5 +11,5 @@ setup(
     author='y-tetsu',
     url='',
     packages=['reversi', 'reversi.BitBoardMethods', 'reversi.strategies', 'reversi.strategies.common', 'reversi.strategies.coordinator', 'reversi.genetic_algorithm'],
-    ext_modules=[Extension('reversi.BitBoardMethods', ['GetBoardInfoFast.pyx', 'GetLegalMovesFast.pyx'])],
+    ext_modules=[Extension('reversi.BitBoardMethods', ['GetBoardInfoFast.cp37-win_amd64.pyd', 'GetLegalMovesFast.cp37-win_amd64.pyd'])],
 )
