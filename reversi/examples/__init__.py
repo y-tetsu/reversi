04eda3e467cfa58acd14ba47b14c5101afb0e49c
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2020-05-21 18:31:17 +0900

    add examples

---
+++ reversi/examples/__init__.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+import glob
+import os
+import shutil
+
+
+def install():
+    src_dir = os.path.dirname(__file__)
+    dst_dir = os.path.join(os.getcwd(), "reversi_examples")
+
+    print("Install reversi examples to {} ...".format(dst_dir))
+
+    shutil.rmtree(dst_dir, ignore_errors=True)
+    os.makedirs(os.path.join(dst_dir, "extra"))
+    os.makedirs(os.path.join(dst_dir, "extra/perl/bottomright"))
+    os.makedirs(os.path.join(dst_dir, "extra/python/topleft"))
+    os.makedirs(os.path.join(dst_dir, "extra/vbscript/randomcorner"))
+    os.makedirs(os.path.join(dst_dir, "extra/sample_input"))
+
+    patterns = ["[0-5]*.py", "*.json", "extra/*.json", "extra/perl/bottomright/*.pl", "extra/python/topleft/*.py", "extra/vbscript/randomcorner/*.vbs", "extra/sample_input/*.txt"]
+
+    for pattern in patterns:
+        srcs = glob.glob(os.path.join(src_dir, pattern))
+
+        for src in srcs:
+            relpath = os.path.relpath(src, src_dir)
+            dst = os.path.join(dst_dir, relpath)
+
+            print("    {}".format(relpath))
+            shutil.copyfile(src, dst)
+
+
+if __name__ == "__main__":
+    install()
