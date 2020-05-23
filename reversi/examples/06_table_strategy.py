c868b98dbe4473e3c3d188e658a26ed3420e4e9c
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2020-05-23 13:58:36 +0900

    add table strategy

--- reversi/examples/06_table_strategy.py
+++ reversi/examples/06_table_strategy.py
@@ -1,45 +1,58 @@
 #!/usr/bin/env python
-"""
-Strategy Customization
-  1. original strategy
-  2. external strategy
+"""Table Strategy
+
+    This is a example of table reversi strategy.
+
+    In the <strategy.Table>,
+    the evaluation value is calculated by multiplying the weighted table on the board
+    and the disk on the board. The highest one will be chosen next move.
+
+    Arg:
+        corner : corner weight
+        c      : c weight
+        a1     : a1 weight
+        a2     : a2 weight
+        b      : b weight
+        x      : x weight
+        o      : o weight
+
+    Table position is below.
+
+    (size=8)
+    ---------------------------------------------------------
+    |corner|  c   |  a2  |  b   |  b   |  a2  |  c   |corner|
+    ---------------------------------------------------------
+    |  c   |  x   |  o   |  o   |  o   |  o   |  x   |  c   |
+    ---------------------------------------------------------
+    |  a2  |  o   |  a1  |  b   |  b   |  a1  |  o   |  a2  |
+    ---------------------------------------------------------
+    |  b   |  o   |  b   |  b   |  b   |  b   |  o   |  b   |
+    ---------------------------------------------------------
+    |  b   |  o   |  b   |  b   |  b   |  b   |  o   |  b   |
+    ---------------------------------------------------------
+    |  a2  |  o   |  a1  |  b   |  b   |  a1  |  o   |  a2  |
+    ---------------------------------------------------------
+    |  c   |  x   |  o   |  o   |  o   |  o   |  x   |  c   |
+    ---------------------------------------------------------
+    |corner|  c   |  a2  |  b   |  b   |  a2  |  c   |corner|
+    ---------------------------------------------------------
 """
 
+import random
 from reversi import Reversi, strategies
 
 
-class Original(strategies.common.AbstractStrategy):
-    """
-    Original strategy
-    """
-    def next_move(self, color, board):
-        """
-        next move
-        """
-        move = None
-        legal_moves = list(board.get_legal_moves(color).keys())
-
-        #-------------------------
-        # 1. Please customize here for original strategy
-        #  1.1 Check your color and board.
-        #  1.2 Decide your next move and set result.
-        # ↓↓↓↓↓
-        move = legal_moves[0]
-        # ↑↑↑↑↑
-        #-------------------------
-
-        return move
-
-
 if __name__ == '__main__':
-    s = {
-        'Original': Original(),
-        #-------------------------
-        # 2. Please customize here for external strategy
-        #     strategies.External('execute command', timeouttime)
-        # ↓↓↓↓↓
-        'External': strategies.External('py -3.7 ./extra/python/topleft/topleft.py', 60),
-        # ↑↑↑↑↑
-        #-------------------------
-    }
-    Reversi(s).start()
+    Reversi(
+        {
+            'Table': strategies.Table(
+                corner=100,
+                c=-20,
+                a1=5,
+                a2=10,
+                b=3,
+                x=-30,
+                o=-5,
+            ),
+        }
+    ).start()
