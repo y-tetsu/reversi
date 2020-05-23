812cfa9874c80f54519850df0ed7627002794f55
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2020-05-23 13:20:36 +0900

    add easy strategy

--- reversi/examples/05_easy_strategy.py
+++ reversi/examples/05_easy_strategy.py
@@ -1,45 +1,65 @@
 #!/usr/bin/env python
-"""
-Strategy Customization
-  1. original strategy
-  2. external strategy
+"""Easy Strategy
+
+    This is a example of easy reversi strategy.
 """
 
+import random
 from reversi import Reversi, strategies
 
 
-class Original(strategies.common.AbstractStrategy):
+class Easy(strategies.common.AbstractStrategy):
+    """Easy
+
+     This is a easy reversi strategy.
+     The specification is as following.
+        - Preferentially get corners
+        - Avoid near corners
+        - Random choice
     """
-    Original strategy
-    """
+
     def next_move(self, color, board):
-        """
-        next move
+        """tuple (x, y): Return next move
+
+        Args:
+            color (str) : 'black' or 'white'
+            board (obj) : Board(BitBoard) object
         """
         move = None
         legal_moves = list(board.get_legal_moves(color).keys())
+        size = board.size
 
-        #-------------------------
-        # 1. Please customize here for original strategy
-        #  1.1 Check your color and board.
-        #  1.2 Decide your next move and set result.
-        # ↓↓↓↓↓
-        move = legal_moves[0]
-        # ↑↑↑↑↑
-        #-------------------------
+        # Preferentially get corners
+        for corner in [(0, 0), (0, size-1), (size-1, 0), (size-1, size-1)]:
+            if corner in legal_moves:
+                move = corner
+                break
+
+        # Aboid near corners
+        else:
+            next_moves = [i for i in legal_moves]
+            near_corners = [
+                             (1, 0),         (size-2, 0),
+                (0, 1),      (1, 1),         (size-2, 1),      (size-1, 1),
+
+                (0, size-2), (1, size-2),    (size-2, size-2), (size-1, size-2),
+                             (1, size-1),    (size-2, size-1),
+            ]
+            for near_corner in near_corners:
+                if near_corner in next_moves:
+                    next_moves.remove(near_corner)
+
+            # Random choice
+            if next_moves:
+                move = random.choice(next_moves)
+            else:
+                move = random.choice(legal_moves)
 
         return move
 
 
 if __name__ == '__main__':
     s = {
-        'Original': Original(),
-        #-------------------------
-        # 2. Please customize here for external strategy
-        #     strategies.External('execute command', timeouttime)
-        # ↓↓↓↓↓
-        'External': strategies.External('py -3.7 ./extra/python/topleft/topleft.py', 60),
-        # ↑↑↑↑↑
-        #-------------------------
+        'Easy': Easy(),
     }
     Reversi(s).start()
