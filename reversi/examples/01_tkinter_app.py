d979ade082c083dd4aca0bdf485d694d5deddab3
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2020-05-23 16:40:57 +0900

    add tkinter application

--- reversi/examples/01_tkinter_app.py
+++ reversi/examples/01_tkinter_app.py
@@ -1,6 +1,55 @@
 #!/usr/bin/env python
-"""
-Reversi GUI Application
+"""Reversi GUI Application using tkinter
+
+    This is a reversi GUI Application using tkinter.
+
+    AI players:
+        (Level 1)
+            Unselfish
+                This player try to take as little as possible.
+
+            Random
+                This player choose a move at random.
+
+            Greedy
+                This player try to take as much as possible.
+
+            SlowStarter
+                This player become Unselfish in the early stages and then Greedy.
+
+        (Level 2)
+            Table
+                This player evaluates the board at the weight table and choose a move.
+                Take as little as possible, aim at the corners and avoid near the corners.
+
+            MonteCarlo
+                This player chooses his moves using the Monte Carlo method.
+
+            MinMax
+                This player chooses a move by reading 2 moves ahead in the minmax method.
+
+            NegaMax
+                This player chooses a move by reading 3 moves ahead in the negamax method.
+
+        (Level 3)
+            AlphaBeta
+                This player chooses a move by reading 4 moves ahead for as long as time in the alpha-beta method.
+
+            Joseki
+                In addition to AlphaBeta, this player chooses a move according to standard tactics.
+
+            FullReading
+                In addition to Joseki, this player chooses a move by reading the stone difference
+                from the last 9 moves to the final phase of the game.
+
+        (Level 4)
+            Iterative
+                This player applies the iterative deepening method to FullReading
+                and reads moves gradually and deeply for as long as it takes.
+
+            Edge
+                In addition to Iterative, this player chooses a move that increases the definite disc
+                by considering the 4-edge pattern.
 """
 
 from reversi import Reversi, strategies
