6ce9b60dd535394eef39fdc9bef280ac65b2424a
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2020-05-23 22:03:45 +0900

    add alpha beta strategy

--- reversi/examples/08_alphabeta_strategy.py
+++ reversi/examples/08_alphabeta_strategy.py
@@ -1,45 +1,53 @@
 #!/usr/bin/env python
-"""
-Strategy Customization
-  1. original strategy
-  2. external strategy
+"""AlphaBeta Strategy
+
+    This is a example of alpha beta reversi strategy.
+
+    In the <strategy.AlphaBeta>,
+    the alpha beta method is used to select the best move by reading the board
+    up to the specified depth according to the specified evaluation method.
+
+    This is an example of reading 4 moves ahead and selecting a move
+    using the Table strategy's board parameters.
+
+    Arg:
+        depth     : depth of reading the move
+        evaluator : how to calculate the evaluation value of the board
+
+    Evaluator_TP:
+        This will return the results of the evaluation of the Table strategy's board parameters
+        and possible number of moves.
+
+        Arg:
+            corner : corner weight
+            c      : c weight
+            a1     : a1 weight
+            a2     : a2 weight
+            b      : b weight
+            x      : x weight
+            o      : o weight
+            wp     : possible number of moves weight
 """
 
 from reversi import Reversi, strategies
-
-
-class Original(strategies.common.AbstractStrategy):
-    """
-    Original strategy
-    """
-    def next_move(self, color, board):
-        """
-        next move
-        """
-        move = None
-        legal_moves = list(board.get_legal_moves(color).keys())
-
-        #-------------------------
-        # 1. Please customize here for original strategy
-        #  1.1 Check your color and board.
-        #  1.2 Decide your next move and set result.
-        # ↓↓↓↓↓
-        move = legal_moves[0]
-        # ↑↑↑↑↑
-        #-------------------------
-
-        return move
+from reversi.strategies.coordinator import Evaluator_TP
 
 
 if __name__ == '__main__':
-    s = {
-        'Original': Original(),
-        #-------------------------
-        # 2. Please customize here for external strategy
-        #     strategies.External('execute command', timeouttime)
-        # ↓↓↓↓↓
-        'External': strategies.External('py -3.7 ./extra/python/topleft/topleft.py', 60),
-        # ↑↑↑↑↑
-        #-------------------------
-    }
-    Reversi(s).start()
+    Reversi(
+        {
+            'AlphaBeta': strategies.AlphaBeta(
+                depth=4,
+                evaluator=Evaluator_TP(
+                    corner=50,
+                    c=-20,
+                    a1=0,
+                    a2=-1,
+                    b=-1,
+                    x=-25,
+                    o=-5,
+                    wp=5,
+                ),
+            ),
+        }
+    ).start()
