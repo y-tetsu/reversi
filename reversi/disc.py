bbdd1d52bff0fd7a95e181bb1a5119b56ad09329
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2020-06-05 18:33:57 +0900

    add test

--- reversi/disc.py
+++ reversi/disc.py
@@ -41,20 +41,3 @@ class DiscFactory:
             ret = Blank('□')
 
         return ret
-
-
-if __name__ == '__main__':
-    from collections import namedtuple
-
-    factory = DiscFactory()
-
-    CheckDisc = namedtuple('CheckDisc', 'color cls mark')
-    black = CheckDisc('black', Black, '〇')
-    white = CheckDisc('white', White, '●')
-    blank = CheckDisc('blank', Blank, '□')
-
-    for check in (black, white, blank):
-        obj = factory.create(check.color)
-        print(type(obj), obj)
-        assert isinstance(obj, check.cls)
-        assert obj == check.mark
