3b0a191738e2d71e70f334027ec01035b69ae138
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2020-06-28 18:13:51 +0900

    fix style

--- tests/strategies/coordinator/test_evaluator.py
+++ tests/strategies/coordinator/test_evaluator.py
@@ -3,8 +3,8 @@
 
 import unittest
 
-from reversi.board import Board, BitBoard
-from reversi.strategies.coordinator import Evaluator_T, Evaluator_TP, Evaluator_TPO, Evaluator_TPOW, Evaluator_PW, Evaluator_N, Evaluator_NW, Evaluator_TPWE, Evaluator_TPWEC, Evaluator_PWE
+from reversi.board import BitBoard
+import reversi.strategies.coordinator as coord
 
 
 class TestEvaluator(unittest.TestCase):
@@ -23,50 +23,50 @@ class TestEvaluator(unittest.TestCase):
         legal_moves_w = board8.get_legal_moves('white', force=True)
 
         # Evaluator_T
-        evaluator = Evaluator_T()
+        evaluator = coord.Evaluator_T()
         score_b = evaluator.evaluate(color='black', board=board8)
         score_w = evaluator.evaluate(color='white', board=board8)
         self.assertEqual(score_b, -22)
         self.assertEqual(score_w, -22)
 
         # Evaluator_TP
-        evaluator = Evaluator_TP()
+        evaluator = coord.Evaluator_TP()
         score_b = evaluator.evaluate(color='black', board=board8, legal_moves_b=legal_moves_b, legal_moves_w=legal_moves_w)
         self.assertEqual(score_b, -17)
 
         # Evaluator_TPO
-        evaluator = Evaluator_TPO()
+        evaluator = coord.Evaluator_TPO()
         score_b = evaluator.evaluate(color='black', board=board8, legal_moves_b=legal_moves_b, legal_moves_w=legal_moves_w)
         self.assertEqual(score_b, -25.25)
 
         # Evaluator_TPOW
-        evaluator = Evaluator_TPOW()
+        evaluator = coord.Evaluator_TPOW()
         score_b = evaluator.evaluate(color='black', board=board8, legal_moves_b=[], legal_moves_w=[])
         self.assertEqual(score_b, -10006)
         score_b = evaluator.evaluate(color='black', board=board8, legal_moves_b=legal_moves_b, legal_moves_w=legal_moves_w)
         self.assertEqual(score_b, -25.25)
 
         # Evaluator_PW
-        evaluator = Evaluator_PW()
+        evaluator = coord.Evaluator_PW()
         score = evaluator.evaluate(board=board8, legal_moves_b=[], legal_moves_w=[])
         self.assertEqual(score, -10006)
         score = evaluator.evaluate(board=board8, legal_moves_b=legal_moves_b, legal_moves_w=legal_moves_w)
         self.assertEqual(score, 5)
 
         # Evaluator_N
-        evaluator = Evaluator_N()
+        evaluator = coord.Evaluator_N()
         score_b = evaluator.evaluate(color='black', board=board8, legal_moves_b=[], legal_moves_w=[])
         self.assertEqual(score_b, -6)
 
         # Evaluator_NW
-        evaluator = Evaluator_NW()
+        evaluator = coord.Evaluator_NW()
         score = evaluator.evaluate(board=board8, legal_moves_b=[], legal_moves_w=[])
         self.assertEqual(score, -10006)
         score = evaluator.evaluate(board=board8, legal_moves_b=legal_moves_b, legal_moves_w=legal_moves_w)
         self.assertEqual(score, -6)
 
         # Evaluator_TPWE
-        evaluator = Evaluator_TPWE()
+        evaluator = coord.Evaluator_TPWE()
         board8._black_bitboard = 0x0000002010003C7E
         legal_moves_b = board8.get_legal_moves('black', True)
         legal_moves_w = board8.get_legal_moves('white', True)
@@ -78,7 +78,7 @@ class TestEvaluator(unittest.TestCase):
         self.assertEqual(score, -61)
 
         # Evaluator_TPWEC
-        evaluator = Evaluator_TPWEC()
+        evaluator = coord.Evaluator_TPWEC()
         board8._black_bitboard = 0x0000002010003C7C
         legal_moves_b = board8.get_legal_moves('black', True)
         legal_moves_w = board8.get_legal_moves('white', True)
@@ -89,7 +89,7 @@ class TestEvaluator(unittest.TestCase):
         self.assertEqual(score, 422)
 
         # Evaluator_PWE
-        evaluator = Evaluator_PWE()
+        evaluator = coord.Evaluator_PWE()
         board8._black_bitboard = 0x0000002010003C7C
         legal_moves_b = board8.get_legal_moves('black', True)
         legal_moves_w = board8.get_legal_moves('white', True)
