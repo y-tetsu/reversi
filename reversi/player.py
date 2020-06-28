f3211b4044324cf9968d16b58fc1967e511747dd
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2020-06-28 11:38:49 +0900

    add test

--- reversi/player.py
+++ reversi/player.py
@@ -1,14 +1,11 @@
-#!/usr/bin/env python
-"""
-プレイヤーの管理
+"""Player
 """
 
 from reversi.disc import DiscFactory
 
 
 class Player:
-    """
-    プレイヤーを管理する
+    """Player
     """
     def __init__(self, color, name, strategy):
         self.color = color
@@ -23,32 +20,7 @@ class Player:
         return self.disc + self.name
 
     def put_disc(self, board):
+        """put_disc
         """
-        次の手を決めて石を置く
-        """
-        self.move = self.strategy.next_move(self.color, board)    # 戦略に基づいた次の一手
-        self.captures =  board.put_disc(self.color, *self.move)  # ボードに手を打つ
-
-
-if __name__ == '__main__':
-    from board import Board
-    from strategies import ConsoleUserInput
-
-    board4 = Board(4)
-    print(board4)
-
-    p1 = Player('black', 'ユーザ1', ConsoleUserInput())
-    p2 = Player('white', 'ユーザ2', ConsoleUserInput())
-
-    for player in [p1, p2]:
-        legal_moves = board4.get_legal_moves(player.color)
-
-        if legal_moves:
-            print(player, 'の番です')
-
-            for index, value in enumerate(legal_moves, 1):
-                coordinate = (chr(value[0] + 97), str(value[1] + 1))
-                print(f'{index:2d}:', coordinate)
-
-            player.put_disc(board4)
-            print(board4)
+        self.move = self.strategy.next_move(self.color, board)   # decide next move by strategy
+        self.captures =  board.put_disc(self.color, *self.move)  # put disc on board
---
+++ tests/test_player.py
@@ -0,0 +1,35 @@
+"""Tests of player.py
+"""
+
+import unittest
+
+from reversi.board import Board
+from reversi.player import Player
+
+
+class TestPlayer(unittest.TestCase):
+    """player
+    """
+    def test_player(self):
+        class TestStrategy:
+            def next_move(self, color, board):
+                return (5, 4)
+
+        board = Board(8)
+        p = Player('black', 'TestPlayer', TestStrategy())
+
+        # init
+        self.assertEqual(p.color, 'black')
+        self.assertEqual(p.disc, board.disc[p.color])
+        self.assertEqual(p.name, 'TestPlayer')
+        self.assertTrue(isinstance(p.strategy, TestStrategy))
+        self.assertEqual(p.move, (None, None))
+        self.assertEqual(p.captures, [])
+
+        # str
+        self.assertEqual(str(p), board.disc[p.color] + 'TestPlayer')
+
+        # put_disc
+        p.put_disc(board)
+        self.assertEqual(p.move, (5, 4))
+        self.assertEqual(p.captures, [(4, 4)])
