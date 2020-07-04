9ee1434ff313656ddf10240d4871fe4febf1453d
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2020-07-05 00:38:37 +0900

    add _undo_64bit

--- reversi/BitBoardMethods/UndoFast.pyx
+++ reversi/BitBoardMethods/UndoFast.pyx
@@ -2,13 +2,55 @@
 """UndoFast
 """
 
+import sys
+
+
+MAXSIZE64 = 2**63 - 1
+
 
 def undo(board):
     """undo
     """
+    if sys.maxsize == MAXSIZE64:
+        _undo_64bit(board)
+    else:
+        _undo(board)
+
+
+cdef inline _undo_64bit(board):
+    """_undo_64bit
+    """
     cdef:
         unsigned int size, disc_num
-        unsigned long long flippable_disc, put
+        unsigned long long flippable_discs, put
+
+    prev = board.prev.pop()
+
+    if prev:
+        size = board.size
+        flippable_discs, disc_num = prev['flippable_discs'], prev['disc_num']
+
+        put = 1 << ((size*size-1)-(prev['y']*size+prev['x']))
+
+        if prev['color'] == 'black':
+            board._black_bitboard ^= put | flippable_discs
+            board._white_bitboard ^= flippable_discs
+            board.score['black'] -= 1 + disc_num
+            board.score['white'] += disc_num
+        else:
+            board._white_bitboard ^= put | flippable_discs
+            board._black_bitboard ^= flippable_discs
+            board.score['black'] += disc_num
+            board.score['white'] -= 1 + disc_num
+
+    board._legal_moves_cache.clear()
+
+
+cdef inline _undo(board):
+    """_undo
+    """
+    cdef:
+        unsigned int size, disc_num
 
     prev = board.prev.pop()
 
