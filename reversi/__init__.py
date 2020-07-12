2c7b0badcd96024ab554d8dd5e12224764c59111
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2020-07-12 22:19:07 +0900

    fix style

--- reversi/__init__.py
+++ reversi/__init__.py
@@ -1,10 +1,28 @@
-#!/usr/bin/env python
-from reversi.board import Board, BitBoard, MIN_BOARD_SIZE, MAX_BOARD_SIZE
-from reversi.player import Player
-from reversi.display import ConsoleDisplay, NoneDisplay, WindowDisplay
-from reversi.game import Game
-from reversi.window import Window
-from reversi.app import Reversi, Reversic
-from reversi.simulator import Simulator
-import reversi.strategies
-import reversi.genetic_algorithm
+from .board import Board, BitBoard, MIN_BOARD_SIZE, MAX_BOARD_SIZE
+from .player import Player
+from .display import ConsoleDisplay, NoneDisplay, WindowDisplay
+from .game import Game
+from .window import Window
+from .app import Reversi, Reversic
+from .simulator import Simulator
+from . import strategies
+from . import genetic_algorithm
+
+
+__all__ = [
+    'Board',
+    'BitBoard',
+    'MIN_BOARD_SIZE',
+    'MAX_BOARD_SIZE',
+    'Player',
+    'ConsoleDisplay',
+    'NoneDisplay',
+    'WindowDisplay',
+    'Game',
+    'Window',
+    'Reversi',
+    'Reversic',
+    'Simulator',
+    'strategies',
+    'genetic_algorithm',
+]
