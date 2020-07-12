b744190af8649c99bb0b8fc8fd8dd8b262ee03cc
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2020-07-12 23:48:58 +0900

    fix style

--- reversi/genetic_algorithm/__init__.py
+++ reversi/genetic_algorithm/__init__.py
@@ -1,2 +1,8 @@
-from reversi.genetic_algorithm.genetic_algorithm import GeneticAlgorithm
-from reversi.genetic_algorithm.chromosome import Chromosome
+from ..genetic_algorithm.chromosome import Chromosome
+from ..genetic_algorithm.genetic_algorithm import GeneticAlgorithm
+
+
+__all__ = [
+    'Chromosome',
+    'GeneticAlgorithm',
+]
