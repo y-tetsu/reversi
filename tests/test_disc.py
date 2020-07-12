72ffded1fb6ee35d2800eb651de9a25ec2dc4070
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2020-07-12 19:03:40 +0900

    update test

--- tests/test_disc.py
+++ tests/test_disc.py
@@ -12,29 +12,29 @@ class TestDisc(unittest.TestCase):
     def test_disc_black_class(self):
         f = DiscFactory()
         black = f.create('black')
-        assert isinstance(black, Black)
+        self.assertIsInstance(black, Black)
 
     def test_disc_black_mark(self):
         f = DiscFactory()
         black = f.create('black')
-        assert black == '〇'
+        self.assertEqual(black, '〇')
 
     def test_disc_white_class(self):
         f = DiscFactory()
         white = f.create('white')
-        assert isinstance(white, White)
+        self.assertIsInstance(white, White)
 
     def test_disc_white_mark(self):
         f = DiscFactory()
         white = f.create('white')
-        assert white == '●'
+        self.assertEqual(white, '●')
 
     def test_disc_blank_class(self):
         f = DiscFactory()
         blank = f.create('blank')
-        assert isinstance(blank, Blank)
+        self.assertIsInstance(blank, Blank)
 
     def test_disc_blank_mark(self):
         f = DiscFactory()
         blank = f.create('blank')
-        assert blank == '□'
+        self.assertEqual(blank, '□')
