affbdc21749f167db9fd58a06a45b2bb424ea53c
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2020-07-12 11:53:48 +0900

    add test

--- reversi/strategies/coordinator/selector.py
+++ reversi/strategies/coordinator/selector.py
@@ -1,21 +1,14 @@
-#!/usr/bin/env python
+"""Selector
 """
-選択的探索
-"""
-
-import sys
-sys.path.append('../../')
 
 from reversi.strategies.common import AbstractSelector
 
 
 class Selector(AbstractSelector):
-    """
-    ボードの打てる場所を返す
+    """Selector
     """
     def select_moves(self, color, board, moves, scores, depth):
-        """
-        手の候補を決める
+        """select_moves
         """
         if moves is None:
             return list(board.get_legal_moves(color, cache=True).keys())
@@ -24,16 +17,16 @@ class Selector(AbstractSelector):
 
 
 class Selector_W(Selector):
-    """
-    ワースト値に基づいて手を絞る
+    """Selector_W
+
+           ワースト値に基づいて手を絞る
     """
     def __init__(self, depth=3, limit=3):
         self.depth = depth
         self.limit = limit
 
     def select_moves(self, color, board, moves, scores, depth):
-        """
-        手の候補を決める
+        """select_moves
         """
         moves = super().select_moves(color, board, moves, scores, depth)
 
@@ -47,55 +40,3 @@ class Selector_W(Selector):
                     moves.remove(worst_move)  # 最もスコアの低い手を削除
 
         return moves
-
-
-if __name__ == '__main__':
-    import os
-    import time
-
-    from board import BitBoard
-    from strategies.common import Timer
-    from strategies.alphabeta import AlphaBeta_TPW
-
-    bitboard8 = BitBoard(8)
-    bitboard8.put_disc('black', 3, 2)
-    print(bitboard8)
-
-    print('--- Test For Selector ---')
-    selector = Selector()
-
-    moves = selector.select_moves('white', bitboard8, None, None, None)
-    print(moves)
-    assert moves == [(2, 2), (4, 2), (2, 4)]
-
-    bitboard8.put_disc('white', 2, 4)
-    bitboard8.put_disc('black', 1, 5)
-    bitboard8.put_disc('white', 1, 4)
-    bitboard8.put_disc('black', 2, 5)
-    bitboard8.put_disc('white', 2, 6)
-    bitboard8.put_disc('black', 1, 6)
-    bitboard8.put_disc('white', 1, 7)
-    print(bitboard8)
-
-    print('--- Test For Selector_W ---')
-    print(bitboard8)
-
-    strategy = AlphaBeta_TPW()
-    selector = Selector_W()
-
-    assert selector.depth == 3
-    assert selector.limit == 3
-
-    moves = bitboard8.get_legal_moves('black')
-
-    print(strategy.__class__.__name__)
-    Timer.set_deadline(strategy.__class__.__name__, -1000000)
-    best_move, scores = strategy.get_best_move('black', bitboard8, moves, 4)
-
-    moves = selector.select_moves('black', bitboard8, None, scores, 2)
-    print(moves)
-    assert moves == [(0, 3), (2, 3), (0, 4), (5, 4), (0, 5), (4, 5), (5, 5), (0, 6), (0, 7), (2, 7)]
-    moves = selector.select_moves('black', bitboard8, None, scores, 5)
-    print(moves)
-    print(scores)
-    assert moves == [(2, 3), (0, 4), (5, 4), (0, 5), (4, 5), (5, 5), (0, 6), (0, 7), (2, 7)]
---
+++ tests/strategies/coordinator/test_selector.py
@@ -0,0 +1,49 @@
+"""Tests of selector.py
+"""
+
+import unittest
+
+from reversi.board import BitBoard
+from reversi.strategies.alphabeta import _AlphaBeta
+from reversi.strategies.coordinator import Selector, Selector_W, Evaluator_TPW
+
+
+class TestSelector(unittest.TestCase):
+    """selector
+    """
+    def test_selector(self):
+        board = BitBoard(8)
+        board.put_disc('black', 3, 2)
+        selector = Selector()
+        moves = selector.select_moves('white', board, None, None, None)
+
+        self.assertEqual(moves, [(2, 2), (4, 2), (2, 4)])
+
+    def test_selector_w(self):
+        board = BitBoard(8)
+        board.put_disc('black', 3, 2)
+        board.put_disc('white', 2, 4)
+        board.put_disc('black', 1, 5)
+        board.put_disc('white', 1, 4)
+        board.put_disc('black', 2, 5)
+        board.put_disc('white', 2, 6)
+        board.put_disc('black', 1, 6)
+        board.put_disc('white', 1, 7)
+        selector = Selector_W(depth=2, limit=4)
+
+        self.assertEqual(selector.depth, 2)
+        self.assertEqual(selector.limit, 4)
+
+        strategy = _AlphaBeta(evaluator=Evaluator_TPW())
+        selector = Selector_W()
+
+        self.assertEqual(selector.depth, 3)
+        self.assertEqual(selector.limit, 3)
+
+        moves = board.get_legal_moves('black')
+        best_move, scores = strategy.get_best_move('black', board, moves, 4)
+        moves = selector.select_moves('black', board, None, scores, 2)
+        self.assertEqual(moves, [(0, 3), (2, 3), (0, 4), (5, 4), (0, 5), (4, 5), (5, 5), (0, 6), (0, 7), (2, 7)])
+
+        moves = selector.select_moves('black', board, None, scores, 5)
+        self.assertEqual(moves, [(2, 3), (0, 4), (5, 4), (0, 5), (4, 5), (5, 5), (0, 6), (0, 7), (2, 7)])
