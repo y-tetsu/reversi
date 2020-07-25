8cc38637de25484604ef9631f42999a2647d74f5
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2020-07-25 22:15:21 +0900

    update get_score

--- reversi/strategies/TableMethods/GetScoreFast.pyx
+++ reversi/strategies/TableMethods/GetScoreFast.pyx
@@ -2,8 +2,43 @@
 """Get Score of Table strategy
 """
 
+import sys
 
-cpdef inline signed int get_score(color, table, board):
+
+MAXSIZE64 = 2**63 - 1
+
+
+def get_score(color, table, board):
+    """get_score
+    """
+    if board.size == 8:
+        if sys.maxsize == MAXSIZE64:
+            return _get_score_size8_64bit(color, table, board)
+
+    return _get_score(color, table, board)
+
+
+cdef inline signed int _get_score_size8_64bit(color, table, board):
+    """get_score
+    """
+    cdef:
+        unsigned int x, y
+        signed int sign = -1, score
+
+    if color == 'black':
+        sign = 1
+
+    board_info = board.get_board_info()
+    score = 0
+
+    for y in range(8):
+        for x in range(8):
+            score += <signed int>table[y][x] * <signed int>board_info[y][x] * sign
+
+    return score
+
+
+cdef inline signed int _get_score(color, table, board):
     """get_score
     """
     cdef:
