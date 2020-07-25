cf3592155ac8a21b4d219fdb4d3b1c38ebc35a97
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2020-07-25 15:52:26 +0900

    update Undo

--- reversi/BitBoardMethods/UndoFast.pyx
+++ reversi/BitBoardMethods/UndoFast.pyx
@@ -27,19 +27,18 @@ cdef inline _undo_size8_64bit(board):
 
     (color, x, y, flippable_discs, disc_num) = board.prev.pop()
 
-    if color:
+    if color == 'black':
         put = <unsigned long long>1 << (63-(y*8+x))
-
-        if color == 'black':
-            board._black_bitboard ^= put | flippable_discs
-            board._white_bitboard ^= flippable_discs
-            board.score['black'] -= 1 + disc_num
-            board.score['white'] += disc_num
-        else:
-            board._white_bitboard ^= put | flippable_discs
-            board._black_bitboard ^= flippable_discs
-            board.score['black'] += disc_num
-            board.score['white'] -= 1 + disc_num
+        board._black_bitboard ^= put | flippable_discs
+        board._white_bitboard ^= flippable_discs
+        board.score['black'] -= 1 + disc_num
+        board.score['white'] += disc_num
+    elif color == 'white':
+        put = <unsigned long long>1 << (63-(y*8+x))
+        board._white_bitboard ^= put | flippable_discs
+        board._black_bitboard ^= flippable_discs
+        board.score['black'] += disc_num
+        board.score['white'] -= 1 + disc_num
 
     return (color, x, y, flippable_discs, disc_num)
 
