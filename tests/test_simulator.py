e74ac910b90375b7afc6c811a071ad0ca2b5a62b
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2020-09-30 18:31:57 +0900

    add test

--- tests/test_simulator.py
+++ tests/test_simulator.py
@@ -7,7 +7,7 @@ import os
 import json
 
 from reversi import Simulator
-from reversi.strategies import Unselfish, Random, Greedy, SlowStarter, Table, RandomOpening, _AlphaBeta
+from reversi.strategies import AbstractStrategy, Unselfish, Random, Greedy, SlowStarter, Table, RandomOpening, _AlphaBeta
 from reversi.strategies.coordinator import Evaluator_TPW
 
 
@@ -166,3 +166,79 @@ class TestSimulator(unittest.TestCase):
         self.assertEqual(lines[15], "AlphaBeta1                |  50.0% |     5     5     0    10")
         self.assertEqual(lines[16], "AlphaBeta2                |  50.0% |     5     5     0    10")
         self.assertEqual(lines[17], "------------------------------------------------------------")
+
+    def test_simulator_draw(self):
+        json_file = './simulator_setting.json'
+        simulator_setting = {
+            "board_size": 4,
+            "matches": 10,
+            "processes": 1,
+            "random_opening": 0,
+        }
+        with open(json_file, 'w') as f:
+            json.dump(simulator_setting, f)
+
+        class Draw(AbstractStrategy):
+            def next_move(self, color, board):
+                depth = board._black_score + board._white_score - 4
+                move = None
+                if depth == 0:
+                    move = (1, 0)
+                elif depth == 1:
+                    move = (0, 0)
+                elif depth == 2:
+                    move = (0, 1)
+                elif depth == 3:
+                    move = (0, 2)
+                elif depth == 4:
+                    move = (0, 3)
+                elif depth == 5:
+                    move = (2, 3)
+                elif depth == 6:
+                    move = (1, 3)
+                elif depth == 7:
+                    move = (2, 0)
+                elif depth == 8:
+                    move = (3, 3)
+                elif depth == 9:
+                    move = (3, 2)
+                elif depth == 10:
+                    move = (3, 0)
+                elif depth == 11:
+                    move = (3, 1)
+
+                return move
+
+        players_info = {
+            'Draw1': Draw(),
+            'Draw2': Draw(),
+        }
+        simulator = Simulator(players_info, json_file)
+
+        os.remove(json_file)
+
+        with captured_stdout() as stdout:
+            simulator.start()
+            print(simulator)
+
+        lines = stdout.getvalue().splitlines()
+        self.assertEqual(lines[0], "processes 1")
+        self.assertEqual(lines[1], "Draw1 Draw2")
+        self.assertEqual(lines[2], "    - Draw1 Draw2 5")
+        self.assertEqual(lines[3], "    - Draw1 Draw2 10")
+        self.assertEqual(lines[4], "Draw2 Draw1")
+        self.assertEqual(lines[5], "    - Draw2 Draw1 5")
+        self.assertEqual(lines[6], "    - Draw2 Draw1 10")
+        self.assertEqual(lines[7], "")
+        self.assertEqual(lines[8], "Size : 4")
+        self.assertEqual(lines[9], "                          | Draw1                     Draw2                    ")
+        self.assertEqual(lines[10], "-------------------------------------------------------------------------------")
+        self.assertEqual(lines[11], "Draw1                     | ------                      0.0%                    ")
+        self.assertEqual(lines[12], "Draw2                     |   0.0%                    ------                    ")
+        self.assertEqual(lines[13], "-------------------------------------------------------------------------------")
+        self.assertEqual(lines[14], "")
+        self.assertEqual(lines[15], "                          | Total  | Win   Lose  Draw  Match")
+        self.assertEqual(lines[16], "------------------------------------------------------------")
+        self.assertEqual(lines[17], "Draw1                     |   0.0% |     0     0    20    20")
+        self.assertEqual(lines[18], "Draw2                     |   0.0% |     0     0    20    20")
+        self.assertEqual(lines[19], "------------------------------------------------------------")
