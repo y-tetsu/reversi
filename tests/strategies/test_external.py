ff27d9ec6a0803eeead3b3dd7abc30333f8d361f
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2020-11-09 12:26:34 +0900

    add test

--- tests/strategies/test_external.py
+++ tests/strategies/test_external.py
@@ -2,9 +2,11 @@
 """
 
 import unittest
+from test.support import captured_stdout
 
 import reversi
 from reversi.strategies.external import External
+from reversi.board import BitBoard
 
 
 class TestExternal(unittest.TestCase):
@@ -14,3 +16,18 @@ class TestExternal(unittest.TestCase):
         external = External()
         self.assertEqual(external.cmd, None)
         self.assertEqual(external.timeouttime, reversi.strategies.external.TIMEOUT_TIME)
+
+    def test_external_next_move_no_cmd_error(self):
+        def error_message(message):
+            print(message)
+
+        board = BitBoard()
+        color = 'black'
+        external = External()
+        external.error_message = error_message
+
+        with captured_stdout() as stdout:
+            external.next_move(color, board)
+
+        lines = stdout.getvalue().splitlines()
+        self.assertEqual(lines[0], "コマンドが設定されていません。")
