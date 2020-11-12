7a8e53300b644d24c54a3bd14e0a82cfa35d85ec
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2020-11-12 12:02:37 +0900

    add test

--- tests/strategies/test_external.py
+++ tests/strategies/test_external.py
@@ -31,3 +31,19 @@ class TestExternal(unittest.TestCase):
 
         lines = stdout.getvalue().splitlines()
         self.assertEqual(lines[0], "コマンドが設定されていません。")
+
+    def test_external_next_move_cmd_timeout_error(self):
+        def error_message(message):
+            print(message)
+
+        board = BitBoard()
+        color = 'black'
+        external = External('more')
+        external.timeouttime = 0
+        external.error_message = error_message
+
+        with captured_stdout() as stdout:
+            external.next_move(color, board)
+
+        lines = stdout.getvalue().splitlines()
+        self.assertEqual(lines[0], "コマンドがタイムアウトしました。")
