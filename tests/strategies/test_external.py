fe9bb425e1bbe01aaee16542775a051c1848d533
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2020-11-13 18:44:53 +0900

    add test

--- tests/strategies/test_external.py
+++ tests/strategies/test_external.py
@@ -3,6 +3,7 @@
 
 import unittest
 from test.support import captured_stdout
+import os
 
 import reversi
 from reversi.strategies.external import External
@@ -19,6 +20,15 @@ class TestExternal(unittest.TestCase):
     def setUp(self):
         self.board = BitBoard()
         self.color = 'black'
+        self.osname = os.name
+
+        with open('./exit1.py', 'w') as wf:
+            wf.write("import sys\n")
+            wf.write("print('Error : exit(9)', file=sys.stderr)\n")
+            wf.write("exit(9)\n")
+
+    def tearDown(self):
+        os.remove('./exit1.py')
 
     def test_external_init(self):
         external = External()
@@ -45,3 +55,16 @@ class TestExternal(unittest.TestCase):
 
         lines = stdout.getvalue().splitlines()
         self.assertEqual(lines[0], "コマンドがタイムアウトしました。")
+
+    def test_external_next_move_cmd_illegal_finish_error(self):
+        cmd = 'py -3.7 ./exit1.py' if self.osname == 'nt' else 'python ./exit1.py'
+        external = External(cmd)
+        external.error_message = error_message
+
+        with captured_stdout() as stdout:
+            external.next_move(self.color, self.board)
+
+        lines = stdout.getvalue().splitlines()
+        self.assertEqual(lines[0], "プロセスが異常終了しました。終了ステータス(9)")
+        self.assertEqual(lines[1], "(標準エラー出力)")
+        self.assertEqual(lines[2], "Error : exit(9)")
