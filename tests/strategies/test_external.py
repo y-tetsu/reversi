a33ad9a0797859685781b78998f3f0e5c7ed4f3e
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2020-11-13 18:20:09 +0900

    update test

--- tests/strategies/test_external.py
+++ tests/strategies/test_external.py
@@ -9,41 +9,39 @@ from reversi.strategies.external import External
 from reversi.board import BitBoard
 
 
+def error_message(message):
+    print(message)
+
+
 class TestExternal(unittest.TestCase):
     """external
     """
+    def setUp(self):
+        self.board = BitBoard()
+        self.color = 'black'
+
     def test_external_init(self):
         external = External()
         self.assertEqual(external.cmd, None)
         self.assertEqual(external.timeouttime, reversi.strategies.external.TIMEOUT_TIME)
 
     def test_external_next_move_no_cmd_error(self):
-        def error_message(message):
-            print(message)
-
-        board = BitBoard()
-        color = 'black'
         external = External()
         external.error_message = error_message
 
         with captured_stdout() as stdout:
-            external.next_move(color, board)
+            external.next_move(self.color, self.board)
 
         lines = stdout.getvalue().splitlines()
         self.assertEqual(lines[0], "コマンドが設定されていません。")
 
     def test_external_next_move_cmd_timeout_error(self):
-        def error_message(message):
-            print(message)
-
-        board = BitBoard()
-        color = 'black'
         external = External('more')
         external.timeouttime = 0
         external.error_message = error_message
 
         with captured_stdout() as stdout:
-            external.next_move(color, board)
+            external.next_move(self.color, self.board)
 
         lines = stdout.getvalue().splitlines()
         self.assertEqual(lines[0], "コマンドがタイムアウトしました。")
