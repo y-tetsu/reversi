d61629f581b5c8c1f353f1789e08c9e64ddcd0e5
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2020-11-13 20:34:50 +0900

    mod test

--- tests/strategies/test_external.py
+++ tests/strategies/test_external.py
@@ -47,54 +47,64 @@ class TestExternal(unittest.TestCase):
         external = External()
         external.error_message = error_message
 
+        move = None
         with captured_stdout() as stdout:
-            external.next_move(self.color, self.board)
+            move = external.next_move(self.color, self.board)
 
         lines = stdout.getvalue().splitlines()
         self.assertEqual(lines[0], "コマンドが設定されていません。")
+        self.assertEqual(move, (self.board.size//2-1, self.board.size//2-1))
 
     def test_external_next_move_cmd_timeout_error(self):
         external = External('more')
         external.timeouttime = 0
         external.error_message = error_message
 
+        move = None
         with captured_stdout() as stdout:
-            external.next_move(self.color, self.board)
+            move = external.next_move(self.color, self.board)
 
         lines = stdout.getvalue().splitlines()
         self.assertEqual(lines[0], "コマンドがタイムアウトしました。")
+        self.assertEqual(move, (self.board.size//2-1, self.board.size//2-1))
 
     def test_external_next_move_cmd_illegal_finish_error(self):
         cmd = 'py -3.7 ./exit1.py' if self.osname == 'nt' else 'python ./exit1.py'
         external = External(cmd)
         external.error_message = error_message
 
+        move = None
         with captured_stdout() as stdout:
-            external.next_move(self.color, self.board)
+            move = external.next_move(self.color, self.board)
 
         lines = stdout.getvalue().splitlines()
         self.assertEqual(lines[0], "プロセスが異常終了しました。終了ステータス(9)")
         self.assertEqual(lines[1], "(標準エラー出力)")
         self.assertEqual(lines[2], "Error : exit(9)")
+        self.assertEqual(move, (self.board.size//2-1, self.board.size//2-1))
 
     def test_external_next_move_cmd_unexpected_output1_error(self):
         cmd = 'py -3.7 ./unexpected1.py' if self.osname == 'nt' else 'python ./unexpected1.py'
         external = External(cmd)
         external.error_message = error_message
 
+        move = None
         with captured_stdout() as stdout:
-            external.next_move(self.color, self.board)
+            move = external.next_move(self.color, self.board)
 
         lines = stdout.getvalue().splitlines()
         self.assertEqual(lines[0], "プログラムからの戻り値が想定外でした(1)。戻り値(unexpected1)")
+        self.assertEqual(move, (self.board.size//2-1, self.board.size//2-1))
 
     def test_external_next_move_cmd_unexpected_output2_error(self):
         cmd = 'py -3.7 ./unexpected2.py' if self.osname == 'nt' else 'python ./unexpected2.py'
         external = External(cmd)
         external.error_message = error_message
 
+        move = None
         with captured_stdout() as stdout:
-            external.next_move(self.color, self.board)
+            move = external.next_move(self.color, self.board)
 
         lines = stdout.getvalue().splitlines()
         self.assertEqual(lines[0], "プログラムからの戻り値が想定外でした(2)。戻り値(a b)")
+        self.assertEqual(move, (self.board.size//2-1, self.board.size//2-1))
