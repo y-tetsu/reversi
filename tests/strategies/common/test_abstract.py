fd7243adf68c831a8c00adcb24006f51c3da7fcf
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2020-11-23 15:36:05 +0900

    add test

---
+++ tests/strategies/common/test_abstract.py
@@ -0,0 +1,27 @@
+"""Tests of abstract.py
+"""
+
+import unittest
+import os
+import time
+
+from reversi.strategies.common import AbstractStrategy, AbstractScorer, AbstractEvaluator, AbstractOrderer, AbstractSelector
+
+
+class TestAbstract(unittest.TestCase):
+    """abstract
+    """
+    def test_abstract_strategy(self):
+        AbstractStrategy.next_move("self", "color", "board")
+
+    def test_abstract_scorer(self):
+        AbstractScorer.get_score("self", "*args", "**kwargs")
+
+    def test_abstract_evaluator(self):
+        AbstractEvaluator.evaluate("self", "*args", "**kwargs")
+
+    def test_abstract_orderer(self):
+        AbstractOrderer.move_ordering("self", "*args", "**kwargs")
+
+    def test_abstract_selector(self):
+        AbstractSelector.select_moves("self", "*args", "**kwargs")
