fd58fa4df6524fc99b43d3bf199a98146dae628b
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2020-12-19 19:46:46 +0900

    add test_genetic_algorithm_generation_change

--- tests/genetic_algorithm/test_genetic_algorithm.py
+++ tests/genetic_algorithm/test_genetic_algorithm.py
@@ -92,6 +92,39 @@ class Test2(Chromosome):
         return generation, population
 
 
+class Test3(Chromosome):
+    """Test3:cross_over
+    """
+    cnt = 0
+
+    def __init__(self, parameter):
+        self.parameter = parameter
+
+    def fitness(self):
+        return self.parameter
+
+    def reset_fitness(self):
+        pass
+
+    def is_optimal(self):
+        pass
+
+    @classmethod
+    def random_instance(cls):
+        instance = Test3(Test3.cnt)
+        Test3.cnt += 1
+        return instance
+
+    def crossover(self, other):
+        return Test3(100)
+
+    def mutate(self):
+        pass
+
+    def large_mutate(self):
+        pass
+
+
 class RandomSum77(Chromosome):
     """RandomSum77
     """
@@ -139,7 +172,6 @@ class TestGeneticAlgorithm(unittest.TestCase):
     """
     def setUp(self):
         self.json_file = './ga_setting.json'
-
         self.ga_setting = {
             "population_num": 10,
             "offspring_num": 5,
@@ -147,9 +179,19 @@ class TestGeneticAlgorithm(unittest.TestCase):
             "mutation_chance": 0.2,
             "large_mutation": 100
         }
-
         with open(self.json_file, 'w') as f:
             json.dump(self.ga_setting, f)
+
+        self.json_file2 = './ga_setting2.json'
+        self.ga_setting2 = {
+            "population_num": 2,
+            "offspring_num": 2,
+            "max_generations": 10000,
+            "mutation_chance": 1,
+            "large_mutation": 2
+        }
+        with open(self.json_file2, 'w') as f:
+            json.dump(self.ga_setting2, f)
 
         self.population_json = './population.json'
         self.populations1 = {
@@ -228,6 +270,11 @@ class TestGeneticAlgorithm(unittest.TestCase):
         self.assertEqual(ga._generation, 10)
         self.assertEqual(len(ga._population), ga._setting["population_num"])
         self.assertEqual([i.parameter for i in ga._population], [12, 11, 10, 9, 8, 7, 6, 5, 4, 3])
+
+    def test_genetic_algorithm_generation_change(self):
+        ga = GeneticAlgorithm(self.json_file2, Test3)
+        ga._generation_change()
+        self.assertEqual([i.parameter for i in ga._population], [100, 100])
 
     def test_genetic_algorithm_randomsum77(self):
         ga = GeneticAlgorithm(self.json_file, RandomSum77)
