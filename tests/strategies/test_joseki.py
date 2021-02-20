6d8a29a8ed58014c52dce16aff8c9405b4535d62
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2021-02-20 16:52:37 +0900

    add test_usagi_init

--- tests/strategies/test_joseki.py
+++ tests/strategies/test_joseki.py
@@ -2,9 +2,12 @@
 """
 
 import unittest
+import os
 
 from reversi.board import BitBoard
-from reversi.strategies import AbstractStrategy, Random, _Joseki_
+from reversi.strategies import AbstractStrategy, Random, _Joseki_, _Usagi_, Usagi
+from reversi.strategies.common import Measure
+from reversi.strategies.joseki import MOUSE, BULL, TIGER, SROSE, ROSEVILLE, FASTBOAT, CAT, RABBIT
 
 
 class TestJoseki(unittest.TestCase):
@@ -32,3 +35,34 @@ class TestJoseki(unittest.TestCase):
             ('black', 0x0000000810000000, 0x0000001008000000): (5, 4),
         }
         self.assertEqual(joseki.next_move('black', board), (5, 4))
+
+    def test_usagi_init(self):
+        joseki = {}
+        joseki.update(MOUSE)
+        joseki.update(BULL)
+        joseki.update(TIGER)
+        joseki.update(SROSE)
+        joseki.update(ROSEVILLE)
+        joseki.update(FASTBOAT)
+        joseki.update(CAT)
+        joseki.update(RABBIT)
+
+        # no Measure
+        _usagi_ = _Usagi_(Random())
+        key = _usagi_.__class__.__name__ + str(os.getpid())
+        board = BitBoard()
+        _usagi_.next_move('black', board)
+
+        self.assertEqual(_usagi_.joseki, joseki)
+        self.assertIsInstance(_usagi_.base, Random)
+        self.assertFalse(key in Measure.elp_time)
+
+        # with Measure
+        usagi = Usagi(Random())
+        key = usagi.__class__.__name__ + str(os.getpid())
+        board = BitBoard()
+        usagi.next_move('black', board)
+
+        self.assertEqual(usagi.joseki, joseki)
+        self.assertIsInstance(usagi.base, Random)
+        self.assertTrue(key in Measure.elp_time)
