bf0a770bdcc4be4c45260f60735be0875ac9c68b
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2021-02-21 10:49:41 +0900

    add test_cputime

---
+++ tests/strategies/common/test_cputime.py
@@ -0,0 +1,13 @@
+"""Tests of cputime.py
+"""
+
+import unittest
+
+from reversi.strategies.common import CPU_TIME
+
+
+class TestAbstract(unittest.TestCase):
+    """cputime
+    """
+    def test_cputime(self):
+        self.assertEqual(CPU_TIME, 0.5)
