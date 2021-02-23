37fd3bf286b7d965c3a962cd5bedcd04f48250e8
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2021-02-23 19:03:44 +0900

    add test_usagi_next_move

--- tests/strategies/test_joseki.py
+++ tests/strategies/test_joseki.py
@@ -4,6 +4,7 @@
 import unittest
 import os
 
+from reversi import C as c
 from reversi.board import BitBoard
 from reversi.strategies import AbstractStrategy, Random, _Joseki_, _Usagi_, Usagi
 from reversi.strategies.common import Measure
@@ -26,15 +27,15 @@ class TestJoseki(unittest.TestCase):
 
         joseki = _Joseki_(Origin())
         board = BitBoard(4)
-        self.assertEqual(joseki.next_move('black', board), (0, 0))
+        self.assertEqual(joseki.next_move(c.black, board), (0, 0))
 
         board = BitBoard(8)
-        self.assertEqual(joseki.next_move('black', board), (0, 0))
+        self.assertEqual(joseki.next_move(c.black, board), (0, 0))
 
         joseki.joseki = {
-            ('black', 0x0000000810000000, 0x0000001008000000): (5, 4),
+            (c.black, 0x0000000810000000, 0x0000001008000000): (5, 4),
         }
-        self.assertEqual(joseki.next_move('black', board), (5, 4))
+        self.assertEqual(joseki.next_move(c.black, board), (5, 4))
 
     def test_usagi_init(self):
         joseki = {}
@@ -51,7 +52,7 @@ class TestJoseki(unittest.TestCase):
         _usagi_ = _Usagi_(Random())
         key = _usagi_.__class__.__name__ + str(os.getpid())
         board = BitBoard()
-        _usagi_.next_move('black', board)
+        _usagi_.next_move(c.black, board)
 
         self.assertEqual(_usagi_.joseki, joseki)
         self.assertIsInstance(_usagi_.base, Random)
@@ -61,8 +62,33 @@ class TestJoseki(unittest.TestCase):
         usagi = Usagi(Random())
         key = usagi.__class__.__name__ + str(os.getpid())
         board = BitBoard()
-        usagi.next_move('black', board)
+        usagi.next_move(c.black, board)
 
         self.assertEqual(usagi.joseki, joseki)
         self.assertIsInstance(usagi.base, Random)
         self.assertTrue(key in Measure.elp_time)
+
+    def test_usagi_next_move(self):
+        board = BitBoard()
+        usagi = Usagi(Random())
+        patterns = [
+            # turn,   move
+            # --- 兎定石 ---
+            (c.black, (5, 4)),
+            (c.white, (3, 5)),
+            (c.black, (2, 4)),
+            (c.white, (5, 3)),
+            (c.black, (4, 2)),
+            # --- Sローズ基本形 ---
+            (c.white, (2, 5)),
+            (c.black, (3, 2)),
+            (c.white, (5, 5)),
+            (c.black, (4, 5)),
+            (c.white, (3, 6)),
+            (c.black, (6, 2)),
+            (c.white, (2, 3)),
+        ]
+        for turn, expected in patterns:
+            move = usagi.next_move(turn, board)
+            board.put_disc(turn, *move)
+            self.assertEqual(move, expected)
