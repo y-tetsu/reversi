3fbcc4a3e324bfadf74396660fc61c98f236a0eb
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2021-08-13 14:44:39 +0900

    add ScorerMethods

---
+++ reversi/strategies/coordinator/ScorerMethods/__init__.py
@@ -0,0 +1,21 @@
+import os
+import pyximport
+pyximport.install()
+
+
+SLOW_MODE = True
+
+try:
+    if 'FORCE_SCORERMETHODS_IMPORT_ERROR' in os.environ:
+        if os.environ['FORCE_SCORERMETHODS_IMPORT_ERROR'] == 'RAISE':
+            raise ImportError
+
+    from ....strategies.coordinator.ScorerMethods.GetScoreFast import get_blank_score
+    SLOW_MODE = False
+except ImportError:
+    from ....strategies.coordinator.ScorerMethods.GetScore import get_blank_score
+
+
+__all__ = [
+    'get_blank_score',
+]
