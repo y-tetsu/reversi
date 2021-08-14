2d065f5854c8c440394f529abb623a8299ec3c20
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2021-08-14 17:18:45 +0900

    bitboard for w1

--- reversi/strategies/coordinator/ScorerMethods/GetScoreFast.pyx
+++ reversi/strategies/coordinator/ScorerMethods/GetScoreFast.pyx
@@ -2,14 +2,86 @@
 """GetScore
 """
 
+import sys
+
+MAXSIZE64 = 2**63 - 1
+
 
 def get_blank_score(board, w1, w2, w3):
+    if board.size == 8 and sys.maxsize == MAXSIZE64 and hasattr(board, '_black_bitboard'):
+        #return _get_blank_score_size8_64bit(board, w1, w2, w3)
+        ####
+        _get_blank_score_size8_64bit(board, w1, w2, w3)
+        ####
     return _get_blank_score(board, w1, w2, w3)
 
 
 cdef:
     signed int[8] directions_x = [-1, 0, 1, -1, 1, -1, 0, 1]
     signed int[8] directions_y = [-1, -1, -1, 0, 0, 1, 1, 1]
+
+
+cdef inline signed int _get_blank_score_size8_64bit(board, signed int w1, signed int w2, signed int w3):
+    cdef:
+        signed int score = 0
+        unsigned long long black, white, blackwhite
+        unsigned long long horizontal, vertical, diagonal
+
+    black, white = board.get_bitboard_info()
+    blackwhite = black | white
+
+    horizontal = blackwhite & 0x7E7E7E7E7E7E7E7E  # 左右チェック用マスク
+    vertical = blackwhite & 0x00FFFFFFFFFFFF00    # 上下チェック用マスク
+    diagonal = blackwhite & 0x007E7E7E7E7E7E00    # 斜めチェック用マスク
+
+    # 左方向に空がある(右方向が盤面の範囲内)
+    l_blank = horizontal & ((horizontal << 1) & (~blackwhite)) >> 1
+
+    # 右方向に空がある(左方向が盤面の範囲内)
+    r_blank = horizontal & ((horizontal >> 1) & (~blackwhite)) << 1
+
+    # 上方向に空がある(下方向が盤面の範囲内)
+    t_blank = vertical & ((vertical << 8) & (~blackwhite)) >> 8
+
+    # 下方向に空がある(上方向が盤面の範囲内)
+    b_blank = vertical & ((vertical >> 8) & (~blackwhite)) << 8
+
+    # 左上方向に空がある(右下方向が盤面の範囲内)
+    lt_blank = diagonal & ((diagonal << 9) & (~blackwhite)) >> 9
+
+    # 右上方向に空がある(左下方向が盤面の範囲内)
+    rt_blank = diagonal & ((diagonal << 7) & (~blackwhite)) >> 7
+
+    # 左下方向に空がある(右上方向が盤面の範囲内)
+    lb_blank = diagonal & ((diagonal >> 7) & (~blackwhite)) << 7
+
+    # 右下方向に空がある(左上方向が盤面の範囲内)
+    rb_blank = diagonal & ((diagonal >> 9) & (~blackwhite)) << 9
+
+    # w1の計算
+    score += w1 * (_get_bit_count(l_blank & black) - _get_bit_count(l_blank & white))
+    score += w1 * (_get_bit_count(r_blank & black) - _get_bit_count(r_blank & white))
+    score += w1 * (_get_bit_count(t_blank & black) - _get_bit_count(t_blank & white))
+    score += w1 * (_get_bit_count(b_blank & black) - _get_bit_count(b_blank & white))
+    score += w1 * (_get_bit_count(lt_blank & black) - _get_bit_count(lt_blank & white))
+    score += w1 * (_get_bit_count(rt_blank & black) - _get_bit_count(rt_blank & white))
+    score += w1 * (_get_bit_count(lb_blank & black) - _get_bit_count(lb_blank & white))
+    score += w1 * (_get_bit_count(rb_blank & black) - _get_bit_count(rb_blank & white))
+
+    print(board)
+    print('w1 new =', score)
+
+
+cdef inline signed int _get_bit_count(unsigned long long bits):
+    """_get_bit_count
+    """
+    bits = (bits & <unsigned long long>0x5555555555555555) + (bits >> <unsigned int>1 & <unsigned long long>0x5555555555555555)
+    bits = (bits & <unsigned long long>0x3333333333333333) + (bits >> <unsigned int>2 & <unsigned long long>0x3333333333333333)
+    bits = (bits & <unsigned long long>0x0F0F0F0F0F0F0F0F) + (bits >> <unsigned int>4 & <unsigned long long>0x0F0F0F0F0F0F0F0F)
+    bits = (bits & <unsigned long long>0x00FF00FF00FF00FF) + (bits >> <unsigned int>8 & <unsigned long long>0x00FF00FF00FF00FF)
+    bits = (bits & <unsigned long long>0x0000FFFF0000FFFF) + (bits >> <unsigned int>16 & <unsigned long long>0x0000FFFF0000FFFF)
+
+    return (bits & <unsigned long long>0x00000000FFFFFFFF) + (bits >> <unsigned int>32 & <unsigned long long>0x00000000FFFFFFFF)
 
 
 cdef inline signed int _get_blank_score(board, signed int w1, signed int w2, signed int w3):
@@ -20,6 +92,9 @@ cdef inline signed int _get_blank_score(board, signed int w1, signed int w2, sig
         signed int score = 0
         signed int board_info[26][26]
 
+    #####
+    w1cnt = 0
+    #####
     board_info_tmp = board.get_board_info()
     for y in range(size):
         for x in range(size):
@@ -31,6 +106,9 @@ cdef inline signed int _get_blank_score(board, signed int w1, signed int w2, sig
             # 自分または相手の石が存在する
             if board_info[x][y]:
                 value = 0
+                ###
+                valuetmp = 0
+                ###
                 for j in range(8):
                     dx, dy = directions_x[j], directions_y[j]
                     next_x1, next_y1 = x + dx, y + dy
@@ -39,6 +117,9 @@ cdef inline signed int _get_blank_score(board, signed int w1, signed int w2, sig
                         # 空きマスに面している(ただし反対側が盤面の範囲内)
                         if not board_info[next_x1][next_y1]:
                             value += w1
+                            #####
+                            valuetmp += w1
+                            #####
                             # 隅に接している場合
                             d = dy * size + dx
                             if i+d == 0 or i+d == size-1 or i+d == size_x_size-8 or i+d == size_x_size-1:
@@ -54,4 +135,6 @@ cdef inline signed int _get_blank_score(board, signed int w1, signed int w2, sig
                                         if not board_info[next_x3][next_y3]:
                                             value += w3  # 隅の反対の縦横方向に空きマスがある場合
                 score += value * board_info[x][y]
+                w1cnt += valuetmp * board_info[x][y]
+    print('w1 org =', w1cnt)
     return score
