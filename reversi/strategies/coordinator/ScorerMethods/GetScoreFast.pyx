3f8f00fc9f5f6de68c2f320228486bbbc7321bd0
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2021-08-21 16:44:28 +0900

    add _get_blank_score_size8_64bit

--- reversi/strategies/coordinator/ScorerMethods/GetScoreFast.pyx
+++ reversi/strategies/coordinator/ScorerMethods/GetScoreFast.pyx
@@ -9,10 +9,7 @@ MAXSIZE64 = 2**63 - 1
 
 def get_blank_score(board, w1, w2, w3):
     if board.size == 8 and sys.maxsize == MAXSIZE64 and hasattr(board, '_black_bitboard'):
-        #return _get_blank_score_size8_64bit(board, w1, w2, w3)
-        ####
-        _get_blank_score_size8_64bit(board, w1, w2, w3)
-        ####
+        return _get_blank_score_size8_64bit(board, w1, w2, w3)
     return _get_blank_score(board, w1, w2, w3)
 
 
@@ -24,15 +21,19 @@ cdef:
 cdef inline signed int _get_blank_score_size8_64bit(board, signed int w1, signed int w2, signed int w3):
     cdef:
         signed int score = 0
-        unsigned long long black, white, blackwhite
+        unsigned long long black, white, blackwhite, not_blackwhite
         unsigned long long horizontal, vertical, diagonal
+        unsigned long long l_blank, r_blank, t_blank, b_blank, lt_blank, rt_blank, lb_blank, rb_blank
+        unsigned long long lt_x, rt_x, lb_x, rb_x
+        unsigned long long lt_r, lt_b, rt_l, rt_b, lb_t, lb_r, rb_t, rb_l
+        signed int lt_r_sign = 1, lt_b_sign = 1, rt_l_sign = 1, rt_b_sign = 1, lb_t_sign = 1, lb_r_sign = 1, rb_t_sign = 1, rb_l_sign = 1
 
     black, white = board.get_bitboard_info()
     blackwhite = black | white
 
-    horizontal = blackwhite & 0x7E7E7E7E7E7E7E7E  # 左右チェック用マスク
-    vertical = blackwhite & 0x00FFFFFFFFFFFF00    # 上下チェック用マスク
-    diagonal = blackwhite & 0x007E7E7E7E7E7E00    # 斜めチェック用マスク
+    horizontal = blackwhite & <unsigned long long>0x7E7E7E7E7E7E7E7E  # 左右チェック用マスク
+    vertical = blackwhite & <unsigned long long>0x00FFFFFFFFFFFF00    # 上下チェック用マスク
+    diagonal = blackwhite & <unsigned long long>0x007E7E7E7E7E7E00    # 斜めチェック用マスク
 
     not_blackwhite = ~blackwhite
 
@@ -61,103 +62,92 @@ cdef inline signed int _get_blank_score_size8_64bit(board, signed int w1, signed
     rb_blank = diagonal & ((diagonal >> 9) & not_blackwhite) << 9
 
     # w1の計算
-    score1 = 0
-    score1 += w1 * (_get_bit_count(l_blank & black) - _get_bit_count(l_blank & white))
-    score1 += w1 * (_get_bit_count(r_blank & black) - _get_bit_count(r_blank & white))
-    score1 += w1 * (_get_bit_count(t_blank & black) - _get_bit_count(t_blank & white))
-    score1 += w1 * (_get_bit_count(b_blank & black) - _get_bit_count(b_blank & white))
-    score1 += w1 * (_get_bit_count(lt_blank & black) - _get_bit_count(lt_blank & white))
-    score1 += w1 * (_get_bit_count(rt_blank & black) - _get_bit_count(rt_blank & white))
-    score1 += w1 * (_get_bit_count(lb_blank & black) - _get_bit_count(lb_blank & white))
-    score1 += w1 * (_get_bit_count(rb_blank & black) - _get_bit_count(rb_blank & white))
-    score += score1
+    score += w1 * (_get_bit_count(l_blank & black) - _get_bit_count(l_blank & white))
+    score += w1 * (_get_bit_count(r_blank & black) - _get_bit_count(r_blank & white))
+    score += w1 * (_get_bit_count(t_blank & black) - _get_bit_count(t_blank & white))
+    score += w1 * (_get_bit_count(b_blank & black) - _get_bit_count(b_blank & white))
+    score += w1 * (_get_bit_count(lt_blank & black) - _get_bit_count(lt_blank & white))
+    score += w1 * (_get_bit_count(rt_blank & black) - _get_bit_count(rt_blank & white))
+    score += w1 * (_get_bit_count(lb_blank & black) - _get_bit_count(lb_blank & white))
+    score += w1 * (_get_bit_count(rb_blank & black) - _get_bit_count(rb_blank & white))
 
     # w2の計算
-    score2 = 0
-    lt_x = lt_blank & 0x0040000000000000  # 左上のX打ち
+    lt_x = lt_blank & <unsigned long long>0x0040000000000000  # 左上のX打ち
     if lt_x:
         if lt_x & black:
-            score2 += w2
+            score += w2
         else:
-            score2 -= w2
-    rt_x = rt_blank & 0x0002000000000000  # 右上のX打ち
+            score -= w2
+    rt_x = rt_blank & <unsigned long long>0x0002000000000000  # 右上のX打ち
     if rt_x:
         if rt_x & black:
-            score2 += w2
+            score += w2
         else:
-            score2 -= w2
-    lb_x = lb_blank & 0x0000000000004000  # 左下のX打ち
+            score -= w2
+    lb_x = lb_blank & <unsigned long long>0x0000000000004000  # 左下のX打ち
     if lb_x:
         if lb_x & black:
-            score2 += w2
+            score += w2
         else:
-            score2 -= w2
-    rb_x = rb_blank & 0x0000000000000200  # 右下のX打ち
+            score -= w2
+    rb_x = rb_blank & <unsigned long long>0x0000000000000200  # 右下のX打ち
     if rb_x:
         if rb_x & black:
-            score2 += w2
+            score += w2
         else:
-            score2 -= w2
-    score += score2
+            score -= w2
 
     # w3の計算
-    score3 = 0
-    lt_r = l_blank & 0x4000000000000000
-    lt_r_sign = 1 if lt_r & black else -1
-    lt_b = t_blank & 0x0080000000000000
-    lt_b_sign = 1 if lt_b & black else -1
-    rt_l = r_blank & 0x0200000000000000
-    rt_l_sign = 1 if rt_l & black else -1
-    rt_b = t_blank & 0x0001000000000000
-    rt_b_sign = 1 if rt_b & black else -1
-    lb_t = b_blank & 0x0000000000008000
-    lb_t_sign = 1 if lb_t & black else -1
-    lb_r = l_blank & 0x0000000000000040
-    lb_r_sign = 1 if lb_r & black else -1
-    rb_t = b_blank & 0x0000000000000100
-    rb_t_sign = 1 if rb_t & black else -1
-    rb_l = r_blank & 0x0000000000000002
-    rb_l_sign = 1 if rb_l & black else -1
+    lt_r = l_blank & <unsigned long long>0x4000000000000000
+    lt_b = t_blank & <unsigned long long>0x0080000000000000
+    rt_l = r_blank & <unsigned long long>0x0200000000000000
+    rt_b = t_blank & <unsigned long long>0x0001000000000000
+    lb_t = b_blank & <unsigned long long>0x0000000000008000
+    lb_r = l_blank & <unsigned long long>0x0000000000000040
+    rb_t = b_blank & <unsigned long long>0x0000000000000100
+    rb_l = r_blank & <unsigned long long>0x0000000000000002
+    if lt_r & white:
+        lt_r_sign = -1
+    if lt_b & white:
+        lt_b_sign = -1
+    if rt_l & white:
+        rt_l_sign = -1
+    if rt_b & white:
+        rt_b_sign = -1
+    if lb_t & white:
+        lb_t_sign = -1
+    if lb_r & white:
+        lb_r_sign = -1
+    if rb_t & white:
+        rb_t_sign = -1
+    if rb_l & white:
+        rb_l_sign = -1
     for i in range(1, 5):
         lt_r >>= 1
         if lt_r & not_blackwhite:
-            print('lt_r')
-            score3 += w3 * lt_r_sign
+            score += w3 * lt_r_sign
         lt_b >>= 8
         if lt_b & not_blackwhite:
-            print('lt_b')
-            score3 += w3 * lt_b_sign
+            score += w3 * lt_b_sign
         rt_l <<= 1
         if rt_l & not_blackwhite:
-            print('rt_l')
-            score3 += w3 * rt_l_sign
+            score += w3 * rt_l_sign
         rt_b >>= 8
         if rt_b & not_blackwhite:
-            print('rt_b')
-            score3 += w3 * rt_b_sign
+            score += w3 * rt_b_sign
         lb_t <<= 8
         if lb_t & not_blackwhite:
-            print('lb_t')
-            score3 += w3 * lb_t_sign
+            score += w3 * lb_t_sign
         lb_r >>= 1
         if lb_r & not_blackwhite:
-            print('lb_r')
-            score3 += w3 * lb_r_sign
+            score += w3 * lb_r_sign
         rb_t <<= 8
         if rb_t & not_blackwhite:
-            print('rb_t')
-            score3 += w3 * rb_t_sign
+            score += w3 * rb_t_sign
         rb_l <<= 1
         if rb_l & not_blackwhite:
-            print('rb_l')
-            score3 += w3 * rb_l_sign
-    score += score3
-
-    print(board)
-    print('w1    new =', score1)
-    print('w2    new =', score2)
-    print('w3    new =', score3)
-    print('score new =', score)
+            score += w3 * rb_l_sign
+    return score
 
 
 cdef inline signed int _get_bit_count(unsigned long long bits):
@@ -168,7 +158,6 @@ cdef inline signed int _get_bit_count(unsigned long long bits):
     bits = (bits & <unsigned long long>0x0F0F0F0F0F0F0F0F) + (bits >> <unsigned int>4 & <unsigned long long>0x0F0F0F0F0F0F0F0F)
     bits = (bits & <unsigned long long>0x00FF00FF00FF00FF) + (bits >> <unsigned int>8 & <unsigned long long>0x00FF00FF00FF00FF)
     bits = (bits & <unsigned long long>0x0000FFFF0000FFFF) + (bits >> <unsigned int>16 & <unsigned long long>0x0000FFFF0000FFFF)
-
     return (bits & <unsigned long long>0x00000000FFFFFFFF) + (bits >> <unsigned int>32 & <unsigned long long>0x00000000FFFFFFFF)
 
 
@@ -180,11 +169,6 @@ cdef inline signed int _get_blank_score(board, signed int w1, signed int w2, sig
         signed int score = 0
         signed int board_info[26][26]
 
-    #####
-    w1cnt = 0
-    w2cnt = 0
-    w3cnt = 0
-    #####
     board_info_tmp = board.get_board_info()
     for y in range(size):
         for x in range(size):
@@ -196,11 +180,6 @@ cdef inline signed int _get_blank_score(board, signed int w1, signed int w2, sig
             # 自分または相手の石が存在する
             if board_info[x][y]:
                 value = 0
-                ###
-                valuetmp = 0
-                valuetmp2 = 0
-                valuetmp3 = 0
-                ###
                 for j in range(8):
                     dx, dy = directions_x[j], directions_y[j]
                     next_x1, next_y1 = x + dx, y + dy
@@ -209,33 +188,15 @@ cdef inline signed int _get_blank_score(board, signed int w1, signed int w2, sig
                         # 空きマスに面している(ただし反対側が盤面の範囲内)
                         if not board_info[next_x1][next_y1]:
                             value += w1
-                            #####
-                            valuetmp += w1
-                            #####
                             # 隅に接している場合
                             d = dy * size + dx
                             if i+d == 0 or i+d == size-1 or i+d == size_x_size-8 or i+d == size_x_size-1:
                                 if abs(dx) + abs(dy) == 2:
                                     value += w2  # X打ち(チェック方向が斜め)の場合
-                                    #####
-                                    #print(j, '(', dx, dy, ')', '(', next_x1, next_y1, ')', '(', x, y, ')')
-                                    valuetmp2 += w2
-                                    #####
                                 else:
                                     for k in range(1, 5):
                                         next_x3, next_y3 = x - k * dx, y - k * dy
                                         if not board_info[next_x3][next_y3]:
                                             value += w3  # 隅の反対の縦横方向に空きマスがある場合
-                                            #####
-                                            print(next_x3, next_y3)
-                                            valuetmp3 += w3
-                                            #####
                 score += value * board_info[x][y]
-                w1cnt += valuetmp * board_info[x][y]
-                w2cnt += valuetmp2 * board_info[x][y]
-                w3cnt += valuetmp3 * board_info[x][y]
-    print('w1    org =', w1cnt)
-    print('w2    org =', w2cnt)
-    print('w3    org =', w3cnt)
-    print('score org =', score)
     return score
