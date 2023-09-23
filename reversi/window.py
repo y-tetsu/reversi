edcd7b2117b5c98c71850d008dc9973ec7d5af41
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2023-09-23 14:59:40 +0900

    add test code

--- reversi/window.py
+++ reversi/window.py
@@ -14,6 +14,8 @@ import reversi.strategies.TableMethods as TableMethods
 import reversi.strategies.AlphaBetaMethods as AlphaBetaMethods
 import reversi.strategies.NegaScoutMethods as NegaScoutMethods
 
+print('TkVersion :', tk.TkVersion)
+
 WINDOW_TITLE = 'reversi'  # ウィンドウのタイトル
 WINDOW_WIDTH = 1320       # ウィンドウ幅
 WINDOW_HEIGHT = 660       # ウィンドウ高さ
