64d6efb0ea3e55c218aade10fad990fbb89fadbf
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2025-02-01 22:48:11 +0900

    moved to Python3.11. #31

--- reversi/cy/build.bat
+++ reversi/cy/build.bat
@@ -3,3 +3,4 @@ py -3.7 setup.py build_ext --inplace
 py -3.8 setup.py build_ext --inplace
 py -3.9 setup.py build_ext --inplace
 py -3.10 setup.py build_ext --inplace
+py -3.11 setup.py build_ext --inplace
